88189a1e7e9d5ab7601beca744b38b3c9af25779
Yue Sun <yuesu@microsoft.com>
2020-03-04 08:55:25 +0800

    [News] change news main dialog to waterfall (#6)
    * change news main dialog to waterfall
    
    * fix comments
    
    * change this.id to initialDialogId

---
+++ skills/csharp/experimental/newsskill/Models/StateProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsSkill.Models
+{
+    public class StateProperties
+    {
+        public const string GeneralLuisResult = "generalLuisResult";
+        public const string SkillLuisResult = "skillLuisResult";
+    }
+}
