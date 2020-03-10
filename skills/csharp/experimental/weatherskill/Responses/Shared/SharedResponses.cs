abd7f0d9a63ae82020bfb903f13515caf223fe4c
Shiyi Zheng <shzhen@microsoft.com>
2020-03-11 00:26:43 +0800

    remove empty line

--- skills/csharp/experimental/weatherskill/Responses/Shared/SharedResponses.cs
+++ skills/csharp/experimental/weatherskill/Responses/Shared/SharedResponses.cs
@@ -1,4 +1,3 @@
-
 // https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
