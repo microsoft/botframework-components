f6250d2ed708de6f3663c7e12874b66ddc3120d9
Hualiang Xie <hualxie@microsoft.com>
2020-03-11 14:54:18 +0800

    [ITSM] add show action code, ShowThenCloseTest

--- skills/csharp/experimental/itsmskill/Models/Actions/ShowTicketInput.cs
+++ skills/csharp/experimental/itsmskill/Models/Actions/ShowTicketInput.cs
@@ -20,12 +20,32 @@ namespace ITSMSkill.Models.Actions
         [JsonProperty("Urgency")]
         public string Urgency { get; set; }
 
-        public ITSMLuis Convert()
+        public override ITSMLuis CreateLuis()
         {
             var luis = new ITSMLuis
             {
                 Entities = new ITSMLuis._Entities(),
             };
+
+            if (!string.IsNullOrEmpty(Number))
+            {
+                luis.Entities.TicketNumber = new string[] { CovertNumber(Number) };
+            }
+
+            if (!string.IsNullOrEmpty(Search))
+            {
+                luis.Entities.TicketTitle = new string[] { Search };
+            }
+
+            if (!string.IsNullOrEmpty(State))
+            {
+                luis.Entities.TicketState = new string[][] { new string[] { State } };
+            }
+
+            if (!string.IsNullOrEmpty(Urgency))
+            {
+                luis.Entities.UrgencyLevel = new string[][] { new string[] { Urgency } };
+            }
 
             return luis;
         }
