d64ff13c1b568079967ae8cf905d692e59636129
Hualiang Xie <hualxie@microsoft.com>
2020-03-12 12:52:28 +0800

    [ITSM] tool. preserve resx

--- skills/csharp/experimental/itsmskill/Responses/Ticket/TicketResponses.cs
+++ skills/csharp/experimental/itsmskill/Responses/Ticket/TicketResponses.cs
@@ -1,15 +1,13 @@
-﻿// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
+// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
-using Microsoft.Bot.Solutions.Responses;
 
 namespace ITSMSkill.Responses.Ticket
 {
     /// <summary>
     /// Contains bot responses.
     /// </summary>
-    public class TicketResponses : IResponseIdCollection
+    public class TicketResponses
     {
         // Generated accessors
         public const string TicketCreated = "TicketCreated";
