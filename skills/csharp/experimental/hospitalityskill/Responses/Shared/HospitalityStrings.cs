3ef93128d46a07a9916d8b2848fdb69fe85777d2
Hualiang Xie <hualxie@microsoft.com>
2020-03-12 11:46:37 +0800

    [H] tool

---
+++ skills/csharp/experimental/hospitalityskill/Responses/Shared/HospitalityStrings.cs
@@ -0,0 +1,16 @@
+// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace HospitalitySkill.Responses.Shared
+{
+    /// <summary>
+    /// Contains bot responses.
+    /// </summary>
+    public class HospitalityStrings
+    {
+        // Generated accessors
+        public const string ReservationDetails = "ReservationDetails";
+        public const string UpdateReservation = "UpdateReservation";
+    }
+}
