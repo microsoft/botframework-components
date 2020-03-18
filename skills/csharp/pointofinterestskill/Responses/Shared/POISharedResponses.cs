280b7b9340b62c644a08c16192305c634bcdc756
Hualiang Xie <hualxie@microsoft.com>
2020-03-18 16:02:33 +0800

    [POI] remove tt

--- skills/csharp/pointofinterestskill/Responses/Shared/POISharedResponses.cs
+++ skills/csharp/pointofinterestskill/Responses/Shared/POISharedResponses.cs
@@ -1,15 +1,13 @@
-﻿// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
+// https://docs.microsoft.com/en-us/visualstudio/modeling/t4-include-directive?view=vs-2017
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
-using Microsoft.Bot.Solutions.Responses;
 
 namespace PointOfInterestSkill.Responses.Shared
 {
     /// <summary>
     /// Contains bot responses.
     /// </summary>
-    public class POISharedResponses : IResponseIdCollection
+    public class POISharedResponses
     {
         // Generated accessors
         public const string DidntUnderstandMessage = "DidntUnderstandMessage";
