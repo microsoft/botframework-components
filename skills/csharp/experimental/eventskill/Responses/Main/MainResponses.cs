29df066ace5ce5f528eef608e2e232ef8c50c17e
Jing Li <19182479+bobokids@users.noreply.github.com>
2020-03-20 09:58:22 +0800

    [phone] remove .tt file (#111)
    * phone skill remove tt file
    
    * phone skill - remove tt and fix issue 109
    
    * event skill - remove tt files
    
    * phone skill - remove tt files - fix comment

--- skills/csharp/experimental/eventskill/Responses/Main/MainResponses.cs
+++ skills/csharp/experimental/eventskill/Responses/Main/MainResponses.cs
@@ -2,8 +2,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Bot.Solutions.Responses;
-
 namespace EventSkill.Responses.Main
 {
     /// <summary>
