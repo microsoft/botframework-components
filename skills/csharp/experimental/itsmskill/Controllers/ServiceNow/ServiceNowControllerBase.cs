177a65ff3d56c90098807a238197579ed1073b5e
smore <simore@microsoft.com>
2020-04-09 07:11:14 -0700

    [ITSM][ServiceNow] Implement ServiceNow Controller (#140)
    * Implement ServiceNow Controller
    
    * Change To Key in ProactiveModel
    
    * Fix PR comments
    
    * Fix PR comments

---
+++ skills/csharp/experimental/itsmskill/Controllers/ServiceNow/ServiceNowControllerBase.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace ITSMSkill.Controllers.ServiceNow
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using ITSMSkill.Models.ServiceNow;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Bot.Builder;
+
+    public abstract class ServiceNowControllerBase : Controller
+    {
+        protected ServiceNowControllerBase(
+            IMessageReceiver<ServiceNowNotification> messageReceiver,
+            IBotTelemetryClient telemetryClient)
+        {
+            this.TelemetryClient = telemetryClient;
+            this.MessageReceiver = messageReceiver;
+        }
+
+        protected IMessageReceiver<ServiceNowNotification> MessageReceiver { get; }
+
+        protected IBotTelemetryClient TelemetryClient { get; }
+
+        protected async Task<IActionResult> Post(
+        ServiceNowNotification request,
+        CancellationToken cancellationToken)
+        {
+            ServiceResponse result = await this.MessageReceiver
+                .Receive(request, cancellationToken);
+
+            return new ContentResult { StatusCode = (int)result.Code, Content = result.Message };
+        }
+    }
+}
---
+++ skills/csharp/experimental/itsmskill/Models/ServiceNow/ServiceNowNotification.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+
+
+namespace ITSMSkill.Models.ServiceNow
+{
+    using Newtonsoft.Json;
+
+    public class ServiceNowNotification
+    {
+        [JsonProperty]
+        public string Id { get; set; }
+
+        [JsonProperty]
+        public string Title { get; set; }
+
+        [JsonProperty]
+        public string Description { get; set; }
+
+        [JsonProperty]
+        public string Category { get; set; }
+
+        [JsonProperty]
+        public string Impact { get; set; }
+
+        [JsonProperty]
+        public string Urgency { get; set; }
+    }
+}
