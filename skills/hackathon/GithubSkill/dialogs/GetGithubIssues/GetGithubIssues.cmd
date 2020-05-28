7a1b0d0ae45dca7f4b98a0229b575526172281ca
Qiong Wu (qiowu) <qiowu@microsoft.com>
2020-05-28 10:20:03 +0800

    initial checkin (#233)

---
+++ skills/hackathon/GithubSkill/dialogs/GetGithubIssues/GetGithubIssues.cmd
@@ -0,0 +1,1 @@
+D:/Workspace/BotBuilder-Samples/experimental/generation/generator/bin/run dialog:generate:swagger GetGithubIssues.json -o . -r /repos/{owner}/{repo}/issues -m get -p dialog.response -n  GetGithubIssues.schema  --verbose 
