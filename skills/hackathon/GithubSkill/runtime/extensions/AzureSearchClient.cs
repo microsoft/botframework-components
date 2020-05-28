edbb600df39ba5ad54e21716936418af12124dbb
Qiong Wu (qiowu) <qiowu@microsoft.com>
2020-05-28 17:28:32 +0800

    Qiowu/hackathon (#234)
    * initial checkin
    
    * add customized action

---
+++ skills/hackathon/GithubSkill/runtime/extensions/AzureSearchClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Search;
+
+namespace Microsoft.Bot.Solutions.Extensions
+{
+    class AzureSearchClient
+    {
+        public static ISearchIndexClient GetAzureSearchClient(string searchServiceName, string searchServiceAdminApiKey, string searchIndexName)
+        {
+            ISearchServiceClient searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(searchServiceAdminApiKey));
+            return searchClient.Indexes.GetClient(searchIndexName);
+        }
+    }
+}
