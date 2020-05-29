a6e89ccd3d44af791d2bb2f0849e92859760ca8c
Qiong Wu <qiowu@microsoft.com>
2020-05-29 09:11:24 +0800

    merge

---
+++ skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/Models/GitHubIssue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Solutions.Extensions.Common
+{
+    public class GitHubIssue
+    {
+        public GitHubIssue() { }
+        //
+        // Summary:
+        //     The date the issue was last updated.
+        public DateTimeOffset? UpdatedAt { get; set; }
+        //
+        // Summary:
+        //     The date the issue was created.
+        public DateTimeOffset CreatedAt { get; set; }
+        //
+        // Summary:
+        //     The date the issue was closed if closed.
+        public DateTimeOffset? ClosedAt { get; set; }
+
+        public string Body { get; set; }
+        //
+        // Summary:
+        //     Title of the issue
+        public string Title { get; set; }
+
+
+        public string Status { get; set; }
+    }
+}
