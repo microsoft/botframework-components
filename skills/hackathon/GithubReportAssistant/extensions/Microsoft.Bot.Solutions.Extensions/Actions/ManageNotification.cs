5f9d47aa7b2f73e1d517229f71e2fada6c80b983
xieofxie <xieofxie@126.com>
2020-05-29 19:13:20 +0800

    add ManageNotification action & related

---
+++ skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/Actions/ManageNotification.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Solutions.Extensions.Middlewares;
+using Microsoft.Bot.Solutions.Extensions.Utilities;
+using Newtonsoft.Json;
+using ToDoSkill.Services;
+
+namespace Microsoft.Bot.Solutions.Extensions.Actions
+{
+    public class ManageNotification : Dialog
+    {
+        [JsonProperty("$kind")]
+        public const string DeclarativeType = "ManageNotification";
+
+        [JsonConstructor]
+        public ManageNotification([CallerFilePath] string callerPath = "", [CallerLineNumber] int callerLine = 0)
+            : base()
+        {
+            this.RegisterSourceLocation(callerPath, callerLine);
+        }
+
+        [JsonProperty("identifier")]
+        public StringExpression Identifier { get; set; }
+
+        [JsonProperty("activity")]
+        public ITemplate<Activity> Activity { get; set; }
+
+        [JsonProperty("time")]
+        public ObjectExpression<DateTime> Time { get; set; }
+
+        [JsonProperty("repeat")]
+        public IntExpression Repeat { get; set; }
+
+        [JsonProperty("resultProperty")]
+        public string resultProperty { get; set; }
+
+        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
+        {
+            var dcState = dc.State;
+            var identifier = this.Identifier.GetValue(dcState);
+            if (string.IsNullOrEmpty(identifier))
+            {
+                throw new ArgumentNullException(identifier);
+            }
+
+            var result = false;
+            var userReference = dc.Context.TurnState.Get<UserReferenceState>();
+            if (Activity == null)
+            {
+                result = userReference.StopNotification(dc.Context, identifier);
+            }
+            else
+            {
+                var activity = await Activity.BindAsync(dc, dc.State).ConfigureAwait(false);
+                var time = this.Time.GetValue(dcState);
+                var repeat = this.Repeat.GetValue(dcState);
+
+                result = userReference.StartNotification(dc.Context, new UserReferenceState.NotificationOption
+                {
+                    Id = identifier,
+                    Activity = activity,
+                    Time = time,
+                    Repeat = repeat,
+                });
+            }
+
+            if (this.resultProperty != null)
+            {
+                dcState.SetValue(resultProperty, result);
+            }
+
+            // return the actionResult as the result of this operation
+            return await dc.EndDialogAsync(result: result, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/Middlewares/UserReferenceMiddleware.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Solutions.Extensions.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Solutions.Extensions.Middlewares
+{
+    public class UserReferenceMiddleware : IMiddleware
+    {
+        private readonly UserReferenceState userReferenceState;
+
+        public UserReferenceMiddleware(IServiceProvider serviceProvider)
+        {
+            userReferenceState = serviceProvider.GetService<UserReferenceState>();
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken)
+        {
+            turnContext.TurnState.Set(userReferenceState);
+            userReferenceState.Update(turnContext);
+            await next(cancellationToken);
+        }
+    }
+}
