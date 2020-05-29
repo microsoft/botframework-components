f59d9f971f609a8e2868c52100e8cb811798f2dc
Qiong Wu <qiowu@microsoft.com>
2020-05-29 11:57:29 +0800

    add issue list

--- skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/Models/GitHubIssue.cs
+++ skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/Models/GitHubIssue.cs
@@ -7,26 +7,27 @@ namespace Microsoft.Bot.Solutions.Extensions.Common
     public class GitHubIssue
     {
         public GitHubIssue() { }
-        //
-        // Summary:
-        //     The date the issue was last updated.
+
+        public int Id { get; set; }
+
         public DateTimeOffset? UpdatedAt { get; set; }
-        //
-        // Summary:
-        //     The date the issue was created.
+
+
         public DateTimeOffset CreatedAt { get; set; }
-        //
-        // Summary:
-        //     The date the issue was closed if closed.
+
+
         public DateTimeOffset? ClosedAt { get; set; }
 
         public string Body { get; set; }
-        //
-        // Summary:
-        //     Title of the issue
+
+
         public string Title { get; set; }
 
 
         public string Status { get; set; }
+
+
+        public string Url { get; set; }
+        
     }
 }
