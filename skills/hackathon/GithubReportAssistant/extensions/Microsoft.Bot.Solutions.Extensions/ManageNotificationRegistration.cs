9e95430ae7374b285f87a9c7bfbf0d278cff1656
Qiong Wu (qiowu) <qiowu@microsoft.com>
2020-05-31 18:43:48 +0800

    merge with notification code (#245)
    * add ManageNotification action & related
    
    * add sample logic in composer & fixes
    
    * update schema
    
    Co-authored-by: xieofxie <xieofxie@126.com>

---
+++ skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/ManageNotificationRegistration.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Solutions.Extensions.Actions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Microsoft.Bot.Solutions.Extensions
+{
+    public class ManageNotificationRegistration : ComponentRegistration, IComponentDeclarativeTypes
+    {
+        public IEnumerable<DeclarativeType> GetDeclarativeTypes(ResourceExplorer resourceExplorer)
+        {
+            // Actions
+            yield return new DeclarativeType<ManageNotification>(ManageNotification.DeclarativeType);
+        }
+
+        public IEnumerable<JsonConverter> GetConverters(ResourceExplorer resourceExplorer, SourceContext sourceContext)
+        {
+            yield break;
+        }
+    }
+}
---
+++ skills/hackathon/GithubReportAssistant/extensions/Microsoft.Bot.Solutions.Extensions/Models/UserReference.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Solutions.Extensions.Utilities
+{
+    public class UserReference
+    {
+        [JsonConstructor]
+        public UserReference()
+        {
+        }
+
+        public UserReference(ITurnContext turnContext)
+        {
+            Update(turnContext);
+        }
+
+        [JsonIgnore]
+        public IDictionary<string, CancellationTokenSource> CTSs { get; set; } = new Dictionary<string, CancellationTokenSource>();
+
+        public ConversationReference Reference { get; set; }
+
+        public bool Cancel(string id)
+        {
+            if (!string.IsNullOrEmpty(id) && CTSs.ContainsKey(id))
+            {
+                bool hasCancelled = true;
+                var cts = CTSs[id];
+                if (!cts.IsCancellationRequested)
+                {
+                    hasCancelled = false;
+                    cts.Cancel();
+                }
+
+                CTSs.Remove(id);
+                return !hasCancelled;
+            }
+
+            return false;
+        }
+
+        public void Update(ITurnContext turnContext)
+        {
+            Reference = turnContext.Activity.GetConversationReference();
+        }
+    }
+}
