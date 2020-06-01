5c3179a871dcb7b0570f5994baf99f72d3a73d75
xieofxie <xieofxie@126.com>
2020-06-01 18:35:52 +0800

    services.AddSingleton<IStorage>(new MemoryStorage());

--- skills/csharp/emailskill/Startup.cs
+++ skills/csharp/emailskill/Startup.cs
@@ -78,6 +78,8 @@ namespace EmailSkill
             services.AddSingleton(new MicrosoftAppCredentials(settings.MicrosoftAppId, settings.MicrosoftAppPassword));
 
             // Configure bot state
+            // Uncomment the following line for local development without Cosmos Db
+            // services.AddSingleton<IStorage>(new MemoryStorage());
             services.AddSingleton<IStorage>(new CosmosDbPartitionedStorage(settings.CosmosDb));
             services.AddSingleton<UserState>();
             services.AddSingleton<ConversationState>();
--- skills/csharp/experimental/weatherskill/Startup.cs
+++ skills/csharp/experimental/weatherskill/Startup.cs
@@ -81,6 +81,8 @@ namespace WeatherSkill
             services.AddSingleton(new MicrosoftAppCredentials(settings.MicrosoftAppId, settings.MicrosoftAppPassword));
 
             // Configure storage
+            // Uncomment the following line for local development without Cosmos Db
+            // services.AddSingleton<IStorage>(new MemoryStorage());
             services.AddSingleton<IStorage>(new CosmosDbPartitionedStorage(settings.CosmosDb));
             services.AddSingleton<UserState>();
             services.AddSingleton<ConversationState>();
