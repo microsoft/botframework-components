b7613a3ebb46c2db5b3043f69bf82bb9a88dcc9a
xhr0804 <44257569+xhr0804@users.noreply.github.com>
2020-07-06 19:34:51 +0800

    [DCalendar]change search meeting room logic and schema (#272)
    * add tests and fix bugs
    
    * add azure search mock
    
    * merge from mastertest
    
    * fix error
    
    * fix search meeting room logic
    
    Co-authored-by: Huanran Xu <huanx@microsoft.com>

--- skills/declarative/extensions/Microsoft.Bot.Solutions.Extensions/Actions/GetMeetingRooms.cs
+++ skills/declarative/extensions/Microsoft.Bot.Solutions.Extensions/Actions/GetMeetingRooms.cs
@@ -37,8 +37,14 @@ namespace Microsoft.Bot.Solutions.Extensions.Actions
         [JsonProperty("searchIndexName")]
         public StringExpression SearchIndexName { get; set; }
 
-        [JsonProperty("searchQuery")]
-        public StringExpression SearchQuery { get; set; }
+        [JsonProperty("building")]
+        public StringExpression Building { get; set; }
+
+        [JsonProperty("floorNumber")]
+        public IntExpression FloorNumber { get; set; }
+
+        [JsonProperty("meetingRoom")]
+        public StringExpression MeetingRoom { get; set; }
 
         public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
         {
@@ -46,13 +52,29 @@ namespace Microsoft.Bot.Solutions.Extensions.Actions
             var searchServiceAdminApiKey = SearchServiceAdminApiKey.GetValue(dcState);
             var searchServiceName = SearchServiceName.GetValue(dcState);
             var searchIndexName = SearchIndexName.GetValue(dcState);
-            var searchQuery = SearchQuery.GetValue(dcState);
+            var building = Building.GetValue(dcState);
+            var floorNumber = FloorNumber.GetValue(dcState);
+            var meetingRoom = MeetingRoom.GetValue(dcState);
+            
+            var searchQuery = "*";
+            if (building != null && meetingRoom != null)
+            {
+                searchQuery = building + "* " + meetingRoom;
+            }
+            else if (building != null || meetingRoom != null)
+            {
+                searchQuery = (building ?? meetingRoom) + "* ";
+            }
 
+            var searchParameters = new SearchParameters()
+            {
+                Top = meetingRoom != null ? 1 : 10,
+                Filter = floorNumber == 0? null : "FloorNumber eq " + floorNumber
+            };
             var httpHandler = dc.Context.TurnState.Get<HttpMessageHandler>();
             var azureSearchClient = AzureSearchClient.GetAzureSearchClient(searchServiceName, searchServiceAdminApiKey, searchIndexName, httpHandler);
 
-
-            var searchResult = await azureSearchClient.Documents.SearchWithHttpMessagesAsync(searchQuery, new SearchParameters());
+            var searchResult = await azureSearchClient.Documents.SearchWithHttpMessagesAsync(searchQuery, searchParameters);
             
             var result = searchResult.Body.Results.Select(x => x.Document);
 
