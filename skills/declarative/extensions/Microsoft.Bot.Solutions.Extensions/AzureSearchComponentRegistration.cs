ac9616bba31ad7d8b5b2bed2bb1a2d8afb1af906
xhr0804 <44257569+xhr0804@users.noreply.github.com>
2020-07-06 19:35:47 +0800

    fix bugs (#273)
    Co-authored-by: Huanran Xu <huanx@microsoft.com>

---
+++ skills/declarative/extensions/Microsoft.Bot.Solutions.Extensions/AzureSearchComponentRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Solutions.Extensions.Actions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Microsoft.Bot.Solutions.Extensions
+{
+    public class AzureSearchComponentRegistration : ComponentRegistration, IComponentDeclarativeTypes
+    {
+        public IEnumerable<DeclarativeType> GetDeclarativeTypes(ResourceExplorer resourceExplorer)
+        {
+            // Actions
+            yield return new DeclarativeType<GetMeetingRooms>(GetMeetingRooms.DeclarativeType);
+        }
+
+        public IEnumerable<JsonConverter> GetConverters(ResourceExplorer resourceExplorer, SourceContext sourceContext)
+        {
+            yield break;
+        }
+    }
+}
