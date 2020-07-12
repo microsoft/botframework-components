249788a88ae2946eec4759cdd8e40ac3699be55c
Shiyi Zheng <shzhen@microsoft.com>
2020-07-12 21:04:46 +0800

    add sdk

---
+++ skills/declarative/whoskill/runtime/declarative_ut/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using DeclarativeTestLibrary;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Solutions.Extensions;
+
+namespace DeclarativeUT
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            ComponentRegistration.Add(new MSGraphComponentRegistration());
+            return DeclarativeTestBase.HandleMain(args);
+        }
+    }
+}
