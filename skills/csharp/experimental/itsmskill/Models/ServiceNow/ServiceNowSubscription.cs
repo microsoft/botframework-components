d351512b3a3303fcae3ce09f6912b6beb2f2489d
Siddharth More <simore@microsoft.com>
2020-07-14 13:52:43 -0700

    Markdown for Creating BusinessRules and RestMessage

---
+++ skills/csharp/experimental/itsmskill/Models/ServiceNow/ServiceNowSubscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITSMSkill.Models.ServiceNow
+{
+    public class ServiceNowSubscription
+    {
+        public string FilterName { get; set; }
+
+        public string NotificationNameSpace { get; set; }
+
+        public string NotificationApiName { get; set; }
+
+        public string FilterCondition { get; set; }
+    }
+}
