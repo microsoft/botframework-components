d44871b7417690ab15f8a36e36bf7db0196938aa
lauren-mills <lamil@microsoft.com>
2020-09-16 20:58:05 -0700

    [Calendar Skill] Lookup events action tests (#313)
    * Updated customActions and schema
    
    * update dialogs to use getProfile and getWorkingHours actions
    
    * removed default composer tests
    
    * added simple action tests using declarative test model
    
    * cleared values from mock data
    
    * Update GetWorkingHours.cs

---
+++ skills/declarative/calendarSkill/runtime/tests/CalendarSkillTests.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
+{
+    [TestClass]
+    public class CalendarSkillTests
+    {
+        public static ResourceExplorer ResourceExplorer { get; set; }
+
+        public TestContext TestContext { get; set; }
+
+        public static IConfiguration Configuration { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            ResourceExplorer = new ResourceExplorer()
+           .AddFolder(Path.Combine(TestUtils.GetProjectPath(), nameof(CalendarSkillTests)), monitorChanges: false)
+           .AddFolder(Path.Combine(TestUtils.GetProjectPath(), @"..\..\"));
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("testsettings.json", optional: true, reloadOnChange: false)
+                .Build();
+        }
+
+        [TestMethod]
+        public async Task CalendarSkillTests_Greeting()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CalendarSkillTests_GetProfile()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CalendarSkillTests_GetEvents()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CalendarSkillTests_SortEvents()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CalendarSkillTests_GetWorkingHours()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CalendarSkillTests_SettingsTest()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+    }
+}
