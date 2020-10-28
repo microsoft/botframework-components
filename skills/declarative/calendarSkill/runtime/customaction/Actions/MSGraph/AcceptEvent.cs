3ee0ddfeea62cbefc77adab2a1d555f65997e18a
xhr0804 <44257569+xhr0804@users.noreply.github.com>
2020-10-29 07:28:31 +0800

    [CalendarV2] cancel/respond event (#326)
    * squash merge
    
    * fix
    
    * integrate action within detailcard
    
    * fix comments
    
    Co-authored-by: Huanran Xu <huanx@microsoft.com>

---
+++ skills/declarative/calendarSkill/runtime/customaction/Actions/MSGraph/AcceptEvent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+
+namespace Microsoft.BotFramework.Composer.CustomAction.Actions.MSGraph
+{
+    class AcceptEvent : Dialog
+    {
+        [JsonProperty("$kind")]
+        public const string DeclarativeType = "Microsoft.Graph.Calendar.AcceptEvent";
+
+        [JsonConstructor]
+        public AcceptEvent([CallerFilePath] string callerPath = "", [CallerLineNumber] int callerLine = 0)
+            : base()
+        {
+            this.RegisterSourceLocation(callerPath, callerLine);
+        }
+
+        [JsonProperty("resultProperty")]
+        public string ResultProperty { get; set; }
+
+        [JsonProperty("token")]
+        public StringExpression Token { get; set; }
+
+        [JsonProperty("eventId")]
+        public StringExpression EventId { get; set; }
+
+        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
+        {
+            var dcState = dc.State;
+            var token = Token.GetValue(dcState);
+            var eventId = EventId.GetValue(dcState);
+
+            var httpHandler = dc.Context.TurnState.Get<HttpMessageHandler>();
+
+            var httpClient = dc.Context.TurnState.Get<HttpClient>() ?? new HttpClient();
+            var graphClient = MSGraphClient.GetAuthenticatedClient(token, httpClient);
+
+            
+            try
+            {
+                await graphClient.Me.Events[eventId].Accept("accept").Request().PostAsync();
+            }
+            catch (ServiceException ex)
+            {
+                throw MSGraphClient.HandleGraphAPIException(ex);
+            }
+
+            // Write Trace Activity for the http request and response values
+            await dc.Context.TraceActivityAsync(nameof(AcceptEvent), null, valueType: DeclarativeType, label: this.Id).ConfigureAwait(false);
+
+            var result = true;
+
+            if (this.ResultProperty != null)
+            {
+                dcState.SetValue(ResultProperty, result);
+            }
+
+            // return the actionResult as the result of this operation
+            return await dc.EndDialogAsync(result: result, cancellationToken: cancellationToken);
+        }
+    }
+}
