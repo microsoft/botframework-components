1ba295dc63a5780e8aeaa57d6f197c49a84838d5
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-12-08 12:06:46 -0600

    Validating options.  Multi-platform structure.

--- experimental/generator-dotnet-yeoman/generators/app/index.js
+++ experimental/generator-dotnet-yeoman/generators/app/index.js
@@ -5,6 +5,14 @@ const Generator = require('yeoman-generator');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
+INTEGRATION_WEBAPP = 'webapp';
+INTEGRATION_FUNCTIONS = 'functions';
+
+PLATFORM_DOTNET = 'dotnet';
+PLATFORM_JS = 'js';
+PLATFORM_JAVA = 'java';
+PLATFORM_PYTHON = 'python';
+
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
@@ -13,10 +21,32 @@ module.exports = class extends Generator {
             type: String,
             required: true
         });
-        this.argument('integration', {
+
+        this.option('integration', {
+            desc: `The host integration to use:  ${INTEGRATION_WEBAPP} or ${INTEGRATION_FUNCTIONS}`,
             type: String,
-            required: true
+            default: INTEGRATION_WEBAPP,
+            alias: 'i'
         });
+
+        this.option('platform', {
+            desc: `The programming platform to use: ${PLATFORM_DOTNET}`,
+            type: String,
+            default: PLATFORM_DOTNET,
+            alias: 'p'
+        });
+
+        this._verifyOptions();
+    }
+
+    _verifyOptions() {
+        if (this.options.integration != INTEGRATION_WEBAPP && this.options.integration != INTEGRATION_FUNCTIONS) {
+            throw new Error(`--integration must be: ${INTEGRATION_WEBAPP} or ${INTEGRATION_FUNCTIONS}`)
+        }
+
+        if (this.options.platform != PLATFORM_DOTNET) {
+            throw new Error(`--platform must be: ${PLATFORM_DOTNET}`);
+        }
     }
 
     // 1. initializing - Your initialization methods (checking current project state, getting configs, etc)
@@ -29,54 +59,55 @@ module.exports = class extends Generator {
     // 8. end - Called last, cleanup, say good bye, etc
 
     writing() {
-        this._copyBotProject(this.options.integration);
-        this._copyCommon();
-        this._copySolutionFile();
+        this._copyDotnetProject();
+        this._copyAssets();
     }
 
-    _copyBotProject(integration) {
-        if (this.options.integration == "webapp" || this.options.integration == "functions") {
-            const botName = this.options.botName;
-
-            this.fs.copyTpl(
-                this.templatePath(path.join(integration,'**')),
-                this.destinationPath(botName),
-                {
-                    botName
-                }
-            );
-
-            this.fs.move(
-                this.destinationPath(path.join(botName, 'botName.csproj')),
-                this.destinationPath(path.join(botName, `${botName}.csproj`))
-            );
-        }
-    }
-
-    _copyCommon() {
+    _copyDotnetProject() {
         const botName = this.options.botName;
+        const integration = this.options.integration;
+        const platform = this.options.platform;
 
         this.fs.copyTpl(
-            this.templatePath(path.join('common','**')),
+            this.templatePath(path.join(platform, integration,'**')),
             this.destinationPath(botName),
             {
                 botName
             }
         );
+
+        this.fs.move(
+            this.destinationPath(path.join(botName, 'botName.csproj')),
+            this.destinationPath(path.join(botName, `${botName}.csproj`))
+        );
+
+        this._copyDotnetSolutionFile();
     }
 
-    _copySolutionFile() {
+    _copyDotnetSolutionFile() {
         const botName = this.options.botName;
         const botProjectGuid = uuidv4().toUpperCase();
         const solutionGuid = uuidv4().toUpperCase();
 
         this.fs.copyTpl(
-            this.templatePath('botName.sln'),
+            this.templatePath(path.join(this.options.platform, 'botName.sln')),
             this.destinationPath(`${botName}.sln`),
             {
                 botName,
                 botProjectGuid,
                 solutionGuid
+            }
+        );
+    }
+
+    _copyAssets() {
+        const botName = this.options.botName;
+
+        this.fs.copyTpl(
+            this.templatePath(path.join('assets','**')),
+            this.destinationPath(botName),
+            {
+                botName
             }
         );
     }
