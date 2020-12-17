e475d95c66ec681c547201091d327fd8659b3bcc
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-12-17 08:53:05 -0600

    Experimental Runtime samples (#456)
    * Expiremental Runtime samples
    
    * Renamed Runtime samples to scenarios

---
+++ experimental/scenarios/runtime/dotnet/CustomAdapter/CustomAdapter/RuntimeSlackAdapter.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.Adapters.Slack;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Builder.Runtime.Samples
+{
+    public class RuntimeSlackAdapter : SlackAdapter
+    {
+        public RuntimeSlackAdapter(IConfiguration configuration, ConversationState conversationState, UserState userState)
+            : base(configuration)
+        {
+            this.UseBotState(conversationState, userState);
+        }
+    }
+}
