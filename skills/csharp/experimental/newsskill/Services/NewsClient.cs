ef9cff481d55fd9cac928bbce6d044598ed608b2
Thomas Chung <thomas.chung@microsoft.com>
2021-01-12 10:33:02 -0800

    Provision correct resources + allow endpoint override (#478)
    * Provision correct resources + allow endpoint override
    
    * Ensure the NewsClients are disposed properly

--- skills/csharp/experimental/newsskill/Services/NewsClient.cs
+++ skills/csharp/experimental/newsskill/Services/NewsClient.cs
@@ -1,20 +1,48 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Search.NewsSearch;
 using Microsoft.Azure.CognitiveServices.Search.NewsSearch.Models;
 
 namespace NewsSkill.Services
 {
-    public class NewsClient
+    public class HttpUrlRewriteHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            // Rewriting the URI because for the V7 version of the API there is a
+            // migration from Cognitive Service to Bing API URL. However, the relative path
+            // is not the same. So while we can set the EndPoint in the NewsClient we cannot
+            // set the relative path for the REST calls. Doing this to effectively hijack
+            // the request and rewrite the URI to get to the right place.
+            if (request.RequestUri.Host.Contains("api.bing.microsoft.com") &&
+                request.RequestUri.AbsolutePath.Contains("bing/v7.0/news"))
+            {
+                Uri newUri = new Uri(request.RequestUri.AbsoluteUri.Replace("bing/v7.0/news", "v7.0/news"));
+
+                request.RequestUri = newUri;
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+
+    public class NewsClient : IDisposable
     {
         private NewsSearchClient _client;
 
-        public NewsClient(string key)
+        public NewsClient(string endPoint, string key)
         {
-            _client = new NewsSearchClient(new ApiKeyServiceClientCredentials(key));
+            _client = new NewsSearchClient(new ApiKeyServiceClientCredentials(key), new HttpUrlRewriteHandler());
+
+            if (!string.IsNullOrEmpty(endPoint))
+            {
+                _client.Endpoint = endPoint;
+            }
         }
 
         public async Task<IList<NewsArticle>> GetNewsForTopicAsync(string query, string mkt)
@@ -37,6 +65,14 @@ namespace NewsSkill.Services
             // general search by category
             var results = await _client.News.CategoryAsync(category: topic, market: mkt, count: 10);
             return results.Value;
+        }
+
+        /// <summary>
+        /// Dispose of the news http client and all the objects underneath
+        /// </summary>
+        public void Dispose()
+        {
+            this._client.Dispose();
         }
     }
 }
