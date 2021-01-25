fbd82ea18e1a3d33150b1e4b52b50721fec9212f
lauren-mills <lamil@microsoft.com>
2021-01-25 13:33:22 -0800

    Refactor createDialog (#497)
    * started create dialog refactor
    
    * update for composer 1.3
    
    * Update LU
    
    * updated lu files
    
    * removed createDialog
    
    * updated getContactDialog
    
    * Added expectedType to datetimeresolution dialog
    
    * updated CreateEvent dialog
    
    * updated calendarSkill.dialog
    
    * lu updates
    
    * Updated LU (added phrase lists)
    
    * Updated createEventDialog
    
    * Added support for removing attendees
    
    * updated create dialog tests
    
    * updated gitignore
    
    * Updated setDateTime trigger
    
    * added interruption tests for createDialog

--- skills/declarative/calendarSkill/runtime/tests/DialogTests.cs
+++ skills/declarative/calendarSkill/runtime/tests/DialogTests.cs
@@ -119,6 +119,114 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
         }
 
         [TestMethod]
+        public async Task CreateEvent_noEntities()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_withEntity_title()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_withEntity_title_contact()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_withEntity_title_contact_datetime()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_withEntity_contact()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_withEntity_datetime()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_withEntity_location()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setTitle()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setAttendeeAdd()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setAttendeeRemove()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setDateTime()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setDescription()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setDuration()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setOnlineMeetingAdd()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setOnlineMeetingRemove()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_setLocation()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_multiple()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task CreateEvent_interruption_skip()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
         public async Task UpdateEvent()
         {
             await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
