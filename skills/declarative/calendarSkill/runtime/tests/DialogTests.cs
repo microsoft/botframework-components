1924401d12a999dec4090077761c7eac5a586c5c
Lauren Mills <lamil@microsoft.com>
2021-01-27 16:38:08 -0800

    Added getEvents tests

--- skills/declarative/calendarSkill/runtime/tests/DialogTests.cs
+++ skills/declarative/calendarSkill/runtime/tests/DialogTests.cs
@@ -236,5 +236,35 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
         {
             await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
         }
+
+        [TestMethod]
+        public async Task GetEvents_multipleResults()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task GetEvents_noResults()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task GetEvents_singleResult()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task GetEvents_withEntity_contact()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
+
+        [TestMethod]
+        public async Task GetEvents_withEntity_datetime()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
+        }
     }
 }
