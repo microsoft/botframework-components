99c83107f1e57a2d2b12447ccd02587cf3dc0afe
Ben Brown <benbro@microsoft.com>
2021-02-01 14:35:42 -0600

    update multi-bot to latest content

--- experimental/templates/generator-multibot/generators/app/index.js
+++ experimental/templates/generator-multibot/generators/app/index.js
@@ -22,14 +22,36 @@ module.exports = class extends Generator {
 
     // create root bot
     // in the future, this might just be a local template rather than pointing at a child template...
+    // this.composeWith(
+    //   require.resolve('generator-conversational-core/generators/app'),
+    //   {
+    //     arguments: this.args,
+    //   }
+    // );
+
+    // Create the root bot, this is directly deriving from the runtime.
     this.composeWith(
-      require.resolve('generator-conversational-core/generators/app'),
+      require.resolve('generator-adaptive-bot/generators/app'),
       {
         arguments: this.args,
+        packageReferences: [{
+          name: 'Preview.Bot.Component.GeneralIntent',
+          version: '0.0.1-preview2'
+        }, {
+          name: 'Preview.Bot.Component.GreetingDialog',
+          version: '0.0.1-preview3'
+        }, {
+          name: 'Preview.Bot.Component.OnboardingDialog',
+          version: '0.0.1-preview2'
+        }, {
+          name: 'Preview.Bot.Component.UnknownIntentDialog',
+          version: '0.0.1-preview4'
+        }]
       }
     );
 
-    // create skill
+
+    // create skill, this derives from a separate template
     this.composeWith(
       require.resolve('generator-conversational-core/generators/app'),
       {
