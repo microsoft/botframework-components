5354a2fcaef6979ac6c1776e01f845fb4dfd0cae
Ben Brown <benbro@microsoft.com>
2021-02-10 16:20:08 -0600

    remove old stuff

--- experimental/templates/generator-multibot/node_modules/npm-api/lib/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  registry: 'https://registry.npmjs.org/',
-  url: 'https://skimdb.npmjs.com/registry/_design/app/',
-  protocol: 'https',
-  host: 'skimdb.npmjs.com',
-  pathname: 'registry/_design/app'
-};
