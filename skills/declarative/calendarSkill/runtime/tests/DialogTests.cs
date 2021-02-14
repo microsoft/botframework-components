471352514503e787ea5278e4a10cff44dbf54ade
Lauren Mills <lamil@microsoft.com>
2021-02-13 20:32:54 -0800

    updated tests

--- skills/declarative/calendarSkill/runtime/tests/DialogTests.cs
+++ skills/declarative/calendarSkill/runtime/tests/DialogTests.cs
@@ -46,48 +46,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
         }
 
         [TestMethod]
-        [Ignore]
-        public async Task GetEventTitleByAttendees()
-        {
-            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
-        }
-
-        [TestMethod]
-        [Ignore]
-        public async Task GetEventTitleWithoutCondition()
-        {
-            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
-        }
-
-        [TestMethod]
-        [Ignore]
-        public async Task GetEventTitleReprompt()
-        {
-            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
-        }
-
-        [TestMethod]
-        [Ignore]
-        public async Task GetEventTitleNoResult()
-        {
-            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
-        }
-
-        [TestMethod]
-        [Ignore]
-        public async Task GetEventLocationByAttendees()
-        {
-            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
-        }
-
-        [TestMethod]
-        [Ignore]
-        public async Task GetEventLocationOnlineMeeting()
-        {
-            await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
-        }
-
-        [TestMethod]
         public async Task RespondToEvent_Accept()
         {
             await TestUtils.RunTestScript(ResourceExplorer, configuration: Configuration);
