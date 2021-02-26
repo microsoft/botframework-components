7c395f774cfb4f62aa6e5c45d0eb3b294b11bf63
Peter Innes <53846082+peterinnesmsft@users.noreply.github.com>
2021-02-26 14:17:02 -0800

    Updating multibot generator to reference latest adaptive and calendar generators, as well as latest component package versions. (#646)

--- generators/generator-microsoft-bot-multibot/generators/app/index.js
+++ generators/generator-microsoft-bot-multibot/generators/app/index.js
@@ -23,15 +23,15 @@ module.exports = class extends Generator {
         }],
         packageReferences: [{
           name: 'Microsoft.Bot.Components.HelpAndCancel',
-          version: '1.0.0-preview.20210222.79f4c33'
+          version: '1.0.0-preview.20210226.87f28fe'
           },
           {
           name: 'Microsoft.Bot.Components.Welcome',
-          version: '1.0.0-preview.20210222.79f4c33'
+          version: '1.0.0-preview.20210226.87f28fe'
           },
           {
           name: 'Microsoft.Bot.Components.Orchestrator',
-          version: '1.0.0-preview.20210222.79f4c33'
+          version: '1.0.0-preview.20210226.87f28fe'
           },
         ],
         applicationSettingsDirectory: 'settings',
