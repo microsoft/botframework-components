7e1ce0b5b91f839653308f8677151636365d378b
Lauren Mills <lamil@microsoft.com>
2021-02-25 17:35:28 -0800

    updated tests with new responses

--- skills/declarative/tests/calendarSkill/AvailabilityTests.cs
+++ skills/declarative/tests/calendarSkill/AvailabilityTests.cs
@@ -1,42 +1,14 @@
-﻿using System.IO;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder.AI.Luis;
-using Microsoft.Bot.Builder.AI.Luis.Testing;
-using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
-using Microsoft.Extensions.Configuration;
+﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Bot.CalendarSkill.Dialogs.Tests;
 
 namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
 {
     [TestClass]
-    public class AvailabilityTests
+    public class AvailabilityTests : TestBase
     {
-        public static ResourceExplorer ResourceExplorer { get; set; }
-
-        public TestContext TestContext { get; set; }
-
-        public static IConfiguration Configuration { get; set; }
-
-        [ClassInitialize]
-        public static void ClassInitialize(TestContext context)
+        public AvailabilityTests() : base(nameof(AvailabilityTests))
         {
-            var rootFolder = Path.Combine(TestUtils.GetProjectPath(), @"..\..\calendarSkill");
-            var testFolder = Path.Combine(TestUtils.GetProjectPath(), "CalendarSkillTests", nameof(AvailabilityTests));
-            Configuration = new ConfigurationBuilder()
-                .AddJsonFile("testsettings.json", optional: true, reloadOnChange: false)
-                .AddInMemoryCollection(new Dictionary<string, string>
-                    {
-                        { "root", rootFolder},
-                        { "luis:resources", testFolder },
-                    })
-                .UseLuisSettings()
-                .Build();
-
-            ResourceExplorer = new ResourceExplorer()
-           .AddFolder(rootFolder, monitorChanges: false)
-           .AddFolder(testFolder)
-           .RegisterType(LuisAdaptiveRecognizer.Kind, typeof(MockLuisRecognizer), new MockLuisLoader(Configuration));
         }
 
         [TestMethod]
