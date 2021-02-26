453144f8e488cce0437cb8f40b4018d8250d5d0a
Peter Innes <53846082+peterinnesmsft@users.noreply.github.com>
2021-02-26 13:47:07 -0800

    feat: Generator updates (#645)
    * Updating component pacakge versions referenced by calendar and conversational-core generators.
    
    * Updating referenced adaptive generator version as well as new versions of component packages.

--- generators/generator-microsoft-bot-calendar/generators/app/index.js
+++ generators/generator-microsoft-bot-calendar/generators/app/index.js
@@ -16,7 +16,7 @@ module.exports = class extends Generator {
         arguments: this.args,
         packageReferences: [{
           name: 'Microsoft.Bot.Components.Calendar',
-          version: '1.0.0-alpha.20210222.79f4c33'
+          version: '1.0.0-alpha.20210226.87f28fe'
           }],
         pluginDefinitions : [
           {
