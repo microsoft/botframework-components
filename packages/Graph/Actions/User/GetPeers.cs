80de69a04a3ff5f23e202d3fc37a035b46fb27d6
Thomas Chung <thomas.chung@live.com>
2021-03-08 19:21:48 -0800

    Filter out the requestor for peers

--- packages/Graph/Actions/User/GetPeers.cs
+++ packages/Graph/Actions/User/GetPeers.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Bot.Component.Graph.Actions
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using AdaptiveExpressions.Properties;
@@ -59,7 +60,9 @@ namespace Microsoft.Bot.Component.Graph.Actions
             };
 
             GetDirectReports directReportActions = new GetDirectReports();
-            return await directReportActions.CallGraphServiceWithResultAsync(client, newParameters, cancellationToken);
+            IEnumerable<DirectoryObject> result = await directReportActions.CallGraphServiceWithResultAsync(client, newParameters, cancellationToken);
+
+            return result.Where(obj => obj.Id != (string)parameters["UserId"]);
         }
 
         /// <inheritdoc />
