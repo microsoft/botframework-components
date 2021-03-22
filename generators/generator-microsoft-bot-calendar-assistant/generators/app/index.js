68c62f0e91934d26c338d82f181506cb66d28c05
Josh Gummersall <jgummersall@microsoft.com>
2021-03-22 14:36:32 -0700

    fix: sort position

--- generators/generator-microsoft-bot-calendar-assistant/generators/app/index.js
+++ generators/generator-microsoft-bot-calendar-assistant/generators/app/index.js
@@ -30,9 +30,9 @@ module.exports = class extends Generator {
             version: '1.0.0-preview.20210310.8ee9434',
           },
           {
+            isPlugin: true,
             name: 'Microsoft.Bot.Components.Orchestrator',
             version: '1.0.0-preview.20210310.8ee9434',
-            isPlugin: true,
           },
         ],
         modifyApplicationSettings: (appSettings) => {
