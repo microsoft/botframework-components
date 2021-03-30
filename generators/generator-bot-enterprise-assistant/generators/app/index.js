858cd44a16b85930c83d5197755e565391f8b9a7
Lauren Mills <lamil@microsoft.com>
2021-03-30 09:55:27 -0700

    update enterprise assistant generator config

--- generators/generator-bot-enterprise-assistant/generators/app/index.js
+++ generators/generator-bot-enterprise-assistant/generators/app/index.js
@@ -12,6 +12,52 @@ module.exports = class extends BaseGenerator {
       Object.assign(this.options, {
         arguments: this.args,
         applicationSettingsDirectory: 'settings',
+        botProjectSettings: {
+          skills: {
+            calendar: {
+              workspace: '../calendar',
+              remote: false,
+            },
+            people: {
+              workspace: '../people',
+              remote: false,
+            },
+          },
+        },
+        packageReferences: [
+          {
+            name: 'Microsoft.Bot.Components.Welcome',
+            version: '1.0.0-preview.20210324.6dfb4a1',
+          },
+          {
+            isPlugin: true,
+            name: 'Microsoft.Bot.Components.Orchestrator',
+            version: '1.0.0-preview.20210310.a7ff2d0',
+          },
+        ],
+        modifyApplicationSettings: (appSettings) => {
+          Object.assign(appSettings, {
+            skillHostEndpoint: 'http://localhost:3980/api/skills',
+          });
+
+          appSettings.runtimeSettings.features.setSpeak = true;
+        },
+      })
+    );
+
+    // create skill, this derives from a separate template
+    this.composeWith(
+      require.resolve('@microsoft/generator-bot-calendar/generators/app'),
+      Object.assign({}, this.options, {
+        arguments: ['calendar'],
+      })
+    );
+
+    // create skill, this derives from a separate template
+    this.composeWith(
+      require.resolve('@microsoft/generator-bot-people/generators/app'),
+      Object.assign({}, this.options, {
+        arguments: ['people'],
       })
     );
   }
