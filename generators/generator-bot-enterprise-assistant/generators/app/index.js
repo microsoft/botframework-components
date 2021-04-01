a7d5be4c7779a00c4398bc03a5cfd2be211e849a
Lauren Mills <lamil@microsoft.com>
2021-04-01 14:31:14 -0700

    removed helpandcancel package and moved logic into root dialog

--- generators/generator-bot-enterprise-assistant/generators/app/index.js
+++ generators/generator-bot-enterprise-assistant/generators/app/index.js
@@ -34,10 +34,6 @@ module.exports = class extends BaseGenerator {
         },
         packageReferences: [
           {
-            name: 'Microsoft.Bot.Components.HelpAndCancel',
-            version: '1.0.0-preview.20210331.a54d9f1',
-          },
-          {
             isPlugin: true,
             name: 'Microsoft.Bot.Builder.AI.Orchestrator',
             version: '4.13.0-rc1.preview',
