57bd53f24556b14ad73acff7c43602aeaa45eb09
Thomas Chung <thomas.chung@live.com>
2021-04-05 16:33:21 -0700

    Update bot code to be rc2 compliant

--- skills/declarative/whoSkill/whoSkill/Startup.cs
+++ skills/declarative/whoSkill/whoSkill/Startup.cs
@@ -1,11 +1,9 @@
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Runtime.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace whoSkill
 {
@@ -13,7 +11,7 @@ namespace whoSkill
     {
         public Startup(IConfiguration configuration)
         {
-            this.Configuration = configuration;
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -22,36 +20,25 @@ namespace whoSkill
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers().AddNewtonsoftJson();
-            services.AddBotRuntime(this.Configuration);
-
-            // The workaround below will be removed with next SDK patch, when this bug fix gets released: https://github.com/microsoft/botbuilder-dotnet/issues/5239
-            // In the meantime, we're guaranteed to have CoreAdapter registered as IBotFrameworkHttpAdapter, so look it up and register it as BotAdapter.
-            RegisterCoreBotAdapter(services);
+            services.AddBotRuntime(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-#pragma warning disable CA1801 // Review unused parameters
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-#pragma warning restore CA1801 // Review unused parameters
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseWebSockets();
-            app.UseRouting()
-               .UseEndpoints(endpoints =>
-               {
-                   endpoints.MapControllers();
-               });
-        }
-
-        private static void RegisterCoreBotAdapter(IServiceCollection services)
-        {
-            const string coreBotAdapterName = "CoreBotAdapter";
-
-            services.AddSingleton(sp =>
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
             {
-                var adapters = sp.GetServices<IBotFrameworkHttpAdapter>();
-                return (BotAdapter)adapters.Single(a => typeof(BotAdapter).IsAssignableFrom(a.GetType()) && a.GetType().Name.Contains(coreBotAdapterName));
+                endpoints.MapControllers();
             });
         }
     }
