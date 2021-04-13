a47b750503fa4f1d7df1aef5baed18a678232933
Thomas Chung <thomas.chung@live.com>
2021-04-13 13:49:31 -0700

    Fix get manager test

--- skills/declarative/tests/whoSkill/GetManagerTests.cs
+++ skills/declarative/tests/whoSkill/GetManagerTests.cs
@@ -5,8 +5,8 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
 {
     using Microsoft.Graph;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Threading.Tasks;
 
     [TestClass]
@@ -15,8 +15,8 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
         [TestMethod]
         public async Task GetManager_HappyPath()
         {
-            User manager = this.AddUserProfile("Megan Bowen", "megan@contoso.com", "425-111-1111", "City Center", "Software Engineer", addToSearchResult: false);
-            User me = this.AddUserProfile(name: "Thomas Chung", email: "thomas@contoso.com", phoneNumber: "425-222-2222", officeLocation: "City Center", jobTitle: "Software Engineer II", addToSearchResult: true);
+            User manager = this.AddUserProfile(Guid.Parse("A700C092-BCD0-41F6-9E05-B52EFCF56A1B"), "Megan Bowen", "megan@contoso.com", "425-111-1111", "City Center", "Software Engineer", addToSearchResult: false);
+            User me = this.AddUserProfile(id: Guid.Parse("935F2659-50AB-40ED-AAD2-F43F3885AED7"), name: "Thomas Chung", email: "thomas@contoso.com", phoneNumber: "425-222-2222", officeLocation: "City Center", jobTitle: "Software Engineer II", addToSearchResult: true);
 
             // Setup the tree
             this.SetupUserRequest(manager, null, new List<User>() { me });
@@ -34,7 +34,7 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
         [TestMethod]
         public async Task GetManager_NoManager()
         {
-            User me = this.AddUserProfile(name: "Thomas Chung", email: "thomas@contoso.com", phoneNumber: "425-222-2222", officeLocation: "City Center", jobTitle: "Software Engineer II", addToSearchResult: true);
+            User me = this.AddUserProfile(id: Guid.Parse("4F4581BE-775F-4EAA-A8B4-C2267CAED218"), name: "Thomas Chung", email: "thomas@contoso.com", phoneNumber: "425-222-2222", officeLocation: "City Center", jobTitle: "Software Engineer II", addToSearchResult: true);
             this.SetupUserRequest(me, null, null);
 
             await this.RunTestScriptAsync();
