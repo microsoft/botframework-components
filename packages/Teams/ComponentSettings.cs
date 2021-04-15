6c11eabc466caddd653c4f0a2f2e1c0d8c199d84
Eric Dahlvang <erdahlva@microsoft.com>
2021-04-15 10:17:43 -0700

    Add Teams SSOTokenExchange (#882)
    * Add Teams SSOTokenExchange
    
    * Address feedback
    
    * Update to 4.13 and remove nuget.config
    
    * Revert nuget.config
    
    * Revert nuget.config changes
    
    * Fix merge conflict
    
    Co-authored-by: Peter Innes <peinnes@microsoft.com>

---
+++ packages/Teams/ComponentSettings.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Components.Teams
+{
+    /// <summary>
+    /// Settings for teams features.
+    /// </summary>
+    internal class ComponentSettings
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the runtime should use SSO Middleware.
+        /// </summary>
+        public bool UseSingleSignOnMiddleware { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the Connection Name to use for the single sign on token exchange.
+        /// </summary>
+        public string ConnectionName { get; set; }
+    }
+}
