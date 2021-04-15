80d0d48d34cafcfc2e45123cf7fc4eff0ddc6221
Thomas Chung <thomas.chung@live.com>
2021-04-15 13:24:48 -0700

    People Skill - Migrate Default Memory Scope to Turn-based (#891)
    * Move token to turn memory scope
    
    * Move DirectReport scenarios to turn
    
    * Update manager scenario to use turn-based scope
    
    * Update peers scenario
    
    * Update collaborator scenario
    
    * Update profile to use turn-based scope
    
    * Pass ID instead of full object between dialogs
    
    * Refactor to common resolve user dialog
    
    * Refactor direct report dialog
    
    * Move to listdialog2
    
    * Change to postBack
    
    * Return value from client instead of index
    
    * More bug fixes to use V2 ListDialog
    
    * Get collaborator scenario tests fixes
    
    * Fix user list get profile scenario
    
    * Fix get peers scenario + update tests
    
    * Fix get manager test
    
    * Fix direct report scenario + tests
    
    * More bug fixes
    
    * Fix turn authentication issue on resolve user
    
    * More bug fix
    
    * Stop stuffing result into trace acitivty for Graph actions
    
    * Update emit trace activity names and remove unncessaries
    
    * Rename GetDirectReport/GetManagerDialog
    
    * Rename to GetPeersDialog
    
    * GetCollaborators rename
    
    * Rename to GetProfileDialog
    
    * Update GetDirectReportsDialog lu
    
    * DirectReports

--- skills/declarative/tests/whoSkill/GetDirectReportsTests.cs
+++ skills/declarative/tests/whoSkill/GetDirectReportsTests.cs
@@ -5,8 +5,8 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
 {
     using Microsoft.Graph;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Threading.Tasks;
 
     [TestClass]
@@ -15,8 +15,8 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
         [TestMethod]
         public async Task GetDirectReports_OneFound()
         {
-            User user = this.AddUserProfile("Thomas Chung", "thomas@contoso.com", "111-111-1111", "Redwest A", "PM", true);
-            User directReport = this.AddUserProfile("John Doe", "john@contoso.com", "222-222-2222", "Redwest B", "Director of PM", false);
+            User user = this.AddUserProfile(Guid.Parse("6976FE7F-AA18-4AA4-AECC-05C78DCB37F8"), "Thomas Chung", "thomas@contoso.com", "111-111-1111", "Redwest A", "PM", true);
+            User directReport = this.AddUserProfile(Guid.Parse("4D7FDC00-6FDD-4725-B60A-2A2BEFD4CE42"), "John Doe", "john@contoso.com", "222-222-2222", "Redwest B", "Director of PM", false);
 
             this.SetupUserRequest(user, null, new List<User>() { directReport });
             this.SetupUserRequest(directReport, user, null);
@@ -27,9 +27,9 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
         [TestMethod]
         public async Task GetDirectReports_MoreThanOneFound()
         {
-            User user = this.AddUserProfile("Thomas Chung", "thomas@contoso.com", "111-111-1111", "Redwest A", "PM", true);
-            User directReport1 = this.AddUserProfile("John Doe", "john@contoso.com", "222-222-2222", "Redwest B", "Director of PM", false);
-            User directReport2 = this.AddUserProfile("Jane Doe", "john@contoso.com", "222-222-2222", "Redwest C", "Director of PM", false);
+            User user = this.AddUserProfile(Guid.Parse("F13BB087-1F7E-4304-9AC5-B57787836983"), "Thomas Chung", "thomas@contoso.com", "111-111-1111", "Redwest A", "PM", true);
+            User directReport1 = this.AddUserProfile(Guid.Parse("9CB044E0-478C-4AC6-9E29-1A11A51113C5"), "John Doe", "john@contoso.com", "222-222-2222", "Redwest B", "Director of PM", false);
+            User directReport2 = this.AddUserProfile(Guid.Parse("2A2F9560-0DBE-4F74-B783-243AB82411E4"), "Jane Doe", "john@contoso.com", "222-222-2222", "Redwest C", "Director of PM", false);
 
             this.SetupUserRequest(user, null, new List<User>() { directReport1, directReport2 });
             this.SetupUserRequest(directReport1, user, null);
@@ -47,7 +47,7 @@ namespace Microsoft.Bot.Dialogs.Tests.WhoSkill
         [TestMethod]
         public async Task GetDirectReports_DirectReportsNotFound()
         {
-            User user = this.AddUserProfile("Thomas Chung", "thomas@contoso.com", "111-111-1111", "Redwest A", "PM", true);
+            User user = this.AddUserProfile(Guid.Parse("278B7D87-055C-4572-BEFE-7EB4AC5C7F9D"), "Thomas Chung", "thomas@contoso.com", "111-111-1111", "Redwest A", "PM", true);
             this.SetupUserRequest(user, null, null);
 
             await this.RunTestScriptAsync();
