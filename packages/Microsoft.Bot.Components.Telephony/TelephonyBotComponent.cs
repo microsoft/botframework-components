0b9b6d2f77227e389e4fec606f9b56615894d584
Vanshika Sinha <vasinha@microsoft.com>
2021-04-20 09:23:50 -0700

    Adding Telephony preview package, this includes Call Transfer Activity currently and it will be a place for all telephony extentions for composer

---
+++ packages/Microsoft.Bot.Components.Telephony/TelephonyBotComponent.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Components.Telephony.Actions;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder;
+
+namespace Microsoft.Bot.Components.Telephony
+{
+    public class TeamsBotComponent : BotComponent
+    {
+        /// <inheritdoc/>
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            // Conditionals
+            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<CallTransferDialog>(CallTransferDialog.Kind));
+        }
+    }
+}
