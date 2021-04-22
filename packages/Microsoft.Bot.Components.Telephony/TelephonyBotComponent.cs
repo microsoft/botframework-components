d45c6d9f7397f04cdd04034f218247523406ce1b
Vanshika Sinha <vasinha@microsoft.com>
2021-04-22 16:23:18 -0700

    Revert "addressing comments/error handling/stylecop issues"
    This reverts commit 410c06fa740fca04d8ae2781002391a19c6701b2.

--- packages/Microsoft.Bot.Components.Telephony/TelephonyBotComponent.cs
+++ packages/Microsoft.Bot.Components.Telephony/TelephonyBotComponent.cs
@@ -1,24 +1,21 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Bot.Components.Telephony.Actions;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder;
+
 namespace Microsoft.Bot.Components.Telephony
 {
-    using Microsoft.Bot.Builder;
-    using Microsoft.Bot.Builder.Dialogs.Declarative;
-    using Microsoft.Bot.Components.Telephony.Actions;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-
-    /// <summary>
-    /// Telephony actions registration.
-    /// </summary>
-    public class TelephonyBotComponent : BotComponent
+    public class TeamsBotComponent : BotComponent
     {
         /// <inheritdoc/>
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             // Conditionals
-            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<CallTransfer>(CallTransfer.Kind));
+            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<CallTransferDialog>(CallTransferDialog.Kind));
         }
     }
 }
