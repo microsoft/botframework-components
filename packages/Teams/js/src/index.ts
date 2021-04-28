a0acbf9b359e1bb044f05792c833d229bf30675b
Josh Gummersall <1235378+joshgummersall@users.noreply.github.com>
2021-04-28 11:04:53 -0700

    feat: import Teams Javascript package (#933)
    * move .NET teams code into dotnet directory
    
    * import js adaptive teams package
    
    * resolve PR comments
    
    * resolve dotnet and js schema file diffs
    
    Issue: https://github.com/microsoft/botframework-components/issues/924
    Partial: https://github.com/microsoft/botframework-components/issues/925
    
    * clean up package/tsconfig
    
    * implement commandId filtering
    
    Ref: https://github.com/microsoft/botframework-components/issues/925
    
    * add eslint
    
    * yarn lint --fix
    
    * manual lint fixes
    
    * fix dotnet test reference
    
    * introduce Language build var for JS component
    
    * pull teams uischema changes
    
    * fix teams js keywords

---
+++ packages/Teams/js/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * @module @microsoft/bot-components-teams
+ */
+
+export * from './actions';
+export * from './adaptiveTeamsBotComponent';
+export * from './conditions';
