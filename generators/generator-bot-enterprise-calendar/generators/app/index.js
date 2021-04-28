d15fb22d160a1317859133a132a6b932ab5b093d
Peter Innes <53846082+peterinnesmsft@users.noreply.github.com>
2021-04-28 15:28:46 -0700

    chore: Bumping component dependency versions to 4.13.0-rc7. (#1039)

--- generators/generator-bot-enterprise-calendar/generators/app/index.js
+++ generators/generator-bot-enterprise-calendar/generators/app/index.js
@@ -16,7 +16,7 @@ module.exports = class extends BaseGenerator {
           {
             isPlugin: true,
             name: 'Microsoft.Bot.Components.Graph',
-            version: '1.0.0-rc6',
+            version: '1.0.0-rc7',
           },
         ],
         modifyApplicationSettings: (appSettings) => {
