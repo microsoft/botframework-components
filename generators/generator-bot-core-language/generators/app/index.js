6fc1a44d837939c0341f2b03079994a1c5d0a50d
Peter Innes <peinnes@microsoft.com>
2021-04-29 10:33:34 -0700

    chore: Bumping component dependency versions to 4.13.0-rc8.

--- generators/generator-bot-core-language/generators/app/index.js
+++ generators/generator-bot-core-language/generators/app/index.js
@@ -12,11 +12,11 @@ const packageReferences = {
   [platforms.dotnet]: [
     {
       name: 'Microsoft.Bot.Components.HelpAndCancel',
-      version: '1.0.0-rc7',
+      version: '1.0.0-rc8',
     },
     {
       name: 'Microsoft.Bot.Components.Welcome',
-      version: '1.0.0-rc7',
+      version: '1.0.0-rc8',
     },
   ],
   [platforms.js]: [
