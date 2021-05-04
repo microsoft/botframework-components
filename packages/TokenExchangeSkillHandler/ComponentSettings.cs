976f04144f321d015f140118a79b6d7abb0d122d
Eric Dahlvang <erdahlva@microsoft.com>
2021-05-03 21:37:27 -0700

    Add SSO Token Exchange component

---
+++ packages/TokenExchangeSkillHandler/ComponentSettings.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Components.TokenExchangeSkillHandler
+{
+    internal class ComponentSettings
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the runtime should use the TokenExchangeSkillHandler.
+        /// </summary>
+        public bool UseTokenExchangeSkillHandler { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the Connection Name to use for the single token exchange skill handler.
+        /// </summary>
+        public string TokenExchangeConnectionName { get; set; }
+    }
+}
---
+++ packages/TokenExchangeSkillHandler/SkillsConfiguration.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Bot.Builder.Skills;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Components.TokenExchangeSkillHandler
+{
+    internal class SkillsConfiguration
+    {
+        public SkillsConfiguration(IConfiguration configuration)
+        {
+            var section = configuration?.GetSection("skill");
+            foreach (var child in section.GetChildren())
+            {
+                var props = child.GetChildren().ToDictionary(x => x.Key, x => x.Value);
+
+                Skills.Add(child.Key, new BotFrameworkSkill()
+                {
+                    Id = child.Key,
+                    AppId = props["msAppId"],
+                    SkillEndpoint = new Uri(props["endpointUrl"]),
+                });
+            }
+
+            SkillHostEndpoint = new Uri(configuration.GetValue<string>("skillHostEndpoint"));
+        }
+
+        public Uri SkillHostEndpoint { get; }
+
+        public Dictionary<string, BotFrameworkSkill> Skills { get; } = new Dictionary<string, BotFrameworkSkill>();
+    }
+}
