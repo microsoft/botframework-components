9449d5f58cae42d11613491334839a70467306c4
Peter Innes <peinnes@microsoft.com>
2021-05-05 12:29:49 -0700

    chore: Bumping component dependency versions to 4.13.0-rc9.

--- generators/generator-bot-enterprise-people/generators/app/index.js
+++ generators/generator-bot-enterprise-people/generators/app/index.js
@@ -16,7 +16,7 @@ module.exports = class extends BaseGenerator {
           {
             isPlugin: true,
             name: 'Microsoft.Bot.Components.Graph',
-            version: '1.0.0-rc8',
+            version: '1.0.0-rc9',
           },
         ],
         modifyApplicationSettings: (appSettings) => {
