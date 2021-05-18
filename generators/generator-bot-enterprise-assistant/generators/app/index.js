c2ff29cc32773ff5ac81903f26ee537a68df2b39
tracyboehrer <tracyboehrer@users.noreply.github.com>
2021-05-18 16:43:59 -0500

    [Cherry Pick 4.13] Bump Dotnet and JS SDK dependencies (#1121)
    * bump: js to 4.13.4 (#1120)
    
    * fix: Updated SDK dependencies (#1119)
    
    * Updated SDK dependencies
    
    * Temp fix for missing Adaptive.Test 4.13.2-preview
    
    * Left dotnet packages at 4.13.1, resolved some test dependencies
    
    * Did update dotnet package dependencies to 4.13.2
    
    Co-authored-by: Josh Gummersall <1235378+joshgummersall@users.noreply.github.com>

--- generators/generator-bot-enterprise-assistant/generators/app/index.js
+++ generators/generator-bot-enterprise-assistant/generators/app/index.js
@@ -36,7 +36,7 @@ module.exports = class extends BaseGenerator {
           {
             isPlugin: true,
             name: 'Microsoft.Bot.Builder.AI.Orchestrator',
-            version: '4.13.1',
+            version: '4.13.2',
           },
         ],
         modifyApplicationSettings: (appSettings) => {
