c5bb422940e0faf5a8e0bd687476ec2bdf5c4ff2
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2021-06-11 12:24:57 -0700

    feat: Azure resource deployment for Components Functional Tests pipelines (Empty Bot placeholders)  (#1149)
    * Update createSharedResources.yml
    
    Rename bffn instances to bfcfn to avoid overlap
    
    * Move /tests/functional/build/templates to /build/templates
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Remove DotnetComposer, JS, Python bots
    
    * Remove all DotNet bots except for Simple Host Bot
    
    * Remove additional other language references and rename variables to bfcfn
    
    * Replace bffn with bfcfn
    
    * Update directory of SimpleHostBot
    
    * Rename bot vars to empty bot
    
    * rename additional refs
    
    * remove duplicate app in variables
    
    * add functions to dotnet
    
    * add JS parameters and template for empty bot in webapp/functions
    
    * add prepare_jsgroup step
    
    * fix location to js simple host bot
    
    * Remove python refs
    
    * Add InternalAppSerivcePlanJSName var
    
    * Remove additional Python steps in shared resource creation, bot resource deployment and cleanup
    
    * Change default value of appinsights resource
    
    * Add blog storage deployment to shared resource creation
    
    * add parameter to cli call to deploy storage
    
    * fix casing

--- tests/functional/Bots/Python/Skills/CodeFirst/WaterfallSkillBot/dialogs/cards/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from .card_dialog import CardDialog
-from .card_options import CardOptions
-from .card_sample_helper import CardSampleHelper
-from .channel_supported_cards import ChannelSupportedCards
-
-__all__ = ["CardDialog", "CardOptions", "CardSampleHelper", "ChannelSupportedCards"]
--- tests/functional/Bots/Python/Skills/CodeFirst/WaterfallSkillBot/dialogs/file_upload/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from .file_upload_dialog import FileUploadDialog
-
-__all__ = ["FileUploadDialog"]
