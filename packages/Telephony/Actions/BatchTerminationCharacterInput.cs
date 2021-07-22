888c4db85d3557e92722542c2d73ba7884ac724c
Autumn Wyborny <auwyb@microsoft.com>
2021-07-22 11:51:23 -0700

    squash DTMF batch changes

---
+++ packages/Telephony/Actions/BatchTerminationCharacterInput.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Components.Telephony.Actions
+{
+    /// <summary>
+    /// Aggregates input until it matches a regex pattern and then stores the result in an output property.
+    /// </summary>
+    public class BatchTerminationCharacterInput : RegexAggregatorInput
+    {
+        /// <summary>
+        /// Class identifier.
+        /// </summary>
+        [JsonProperty("$kind")]
+        public new const string Kind = "Microsoft.Telephony.TerminationCharacterBatchInput";
+
+        private const string _dtmfCharacterRegex = @"^[\d#\*]+$";
+        private string _terminationCharacter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BatchTerminationCharacterInput"/> class.
+        /// </summary>
+        /// <param name="regexAggregatorInput">Implementation for aggregatingInput.</param>
+        /// <param name="sourceFilePath">Optional, source file full path.</param>
+        /// <param name="sourceLineNumber">Optional, line number in source file.</param>
+        [JsonConstructor]
+        public BatchTerminationCharacterInput([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+            : base()
+        {
+            // enable instances of this command as debug break point
+            this.RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+
+        /// <summary>
+        /// Gets or sets the character that will be used to signal that the batch of input is complete.
+        /// </summary>
+        [JsonProperty("terminationCharacter")]
+        public string TerminationCharacter
+        {
+            get
+            {
+                return _terminationCharacter;
+            }
+
+            set
+            {
+                TerminationConditionRegexPattern = value + "$";
+                _terminationCharacter = value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override Task<DialogTurnResult> ContinueDialogAsync(DialogContext dc, CancellationToken cancellationToken = default)
+        {
+            if (Regex.Match(dc.Context.Activity.Text, _dtmfCharacterRegex).Success)
+            {
+                return base.ContinueDialogAsync(dc, cancellationToken);
+            }
+            else
+            {
+                return Task.FromResult(new DialogTurnResult(DialogTurnStatus.Waiting));
+            }
+        }
+    }
+}
