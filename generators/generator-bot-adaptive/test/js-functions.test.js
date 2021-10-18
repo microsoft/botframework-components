0f4af009be5e41172cedd18b78e3bb5ce06c7d1d
Ryan Lengel Isgrig <rylengel@microsoft.com>
2021-10-18 13:51:12 -0700

    fix refs to js preview sdks

--- generators/generator-bot-adaptive/test/js-functions.test.js
+++ generators/generator-bot-adaptive/test/js-functions.test.js
@@ -107,8 +107,8 @@ describe(`generator-bot-adaptive --platform ${platform} --integration ${integrat
       },
       dependencies: {
         'cross-env': 'latest',
-        'botbuilder-ai-luis': '4.15.0-rc0',
-        'botbuilder-ai-qna': '4.15.0-rc0',
+        'botbuilder-ai-luis': '4.15.0-rc0.preview',
+        'botbuilder-ai-qna': '4.15.0-rc0.preview',
         'botbuilder-dialogs-adaptive-runtime-integration-azure-functions':
           '4.15.0-rc0.preview',
       },
