9608108deb8a4ea54f967f7534354844b0b390b7
Ryan Lengel Isgrig <rylengel@microsoft.com>
2021-11-19 14:47:52 -0800

    bump internal component refs to 1.2.0

--- generators/generator-bot-core-language/generators/app/index.js
+++ generators/generator-bot-core-language/generators/app/index.js
@@ -4,40 +4,40 @@
 'use strict';
 
 const {
-  BaseGenerator,
-  platforms,
+    BaseGenerator,
+    platforms,
 } = require('@microsoft/generator-bot-adaptive');
 
 const packageReferences = {
-  [platforms.dotnet.name]: [
-    {
-      name: 'Microsoft.Bot.Components.HelpAndCancel',
-      version: '1.2.0-rc2',
-    },
-    {
-      name: 'Microsoft.Bot.Components.Welcome',
-      version: '1.2.0-rc2',
-    },
-  ],
-  [platforms.js.name]: [
-    { name: '@microsoft/bot-components-helpandcancel', version: 'latest' },
-    { name: '@microsoft/bot-components-welcome', version: 'latest' },
-  ],
+    [platforms.dotnet.name]: [
+        {
+            name: 'Microsoft.Bot.Components.HelpAndCancel',
+            version: '1.2.0',
+        },
+        {
+            name: 'Microsoft.Bot.Components.Welcome',
+            version: '1.2.0',
+        },
+    ],
+    [platforms.js.name]: [
+        { name: '@microsoft/bot-components-helpandcancel', version: 'latest' },
+        { name: '@microsoft/bot-components-welcome', version: 'latest' },
+    ],
 };
 
 module.exports = class extends BaseGenerator {
-  initializing() {
-    this.composeWith(
-      require.resolve('@microsoft/generator-bot-adaptive/generators/app'),
-      Object.assign(this.options, {
-        arguments: this.args,
-        applicationSettingsDirectory: 'settings',
-        packageReferences: packageReferences[this.options.platform],
-      })
-    );
-  }
+    initializing() {
+        this.composeWith(
+            require.resolve('@microsoft/generator-bot-adaptive/generators/app'),
+            Object.assign(this.options, {
+                arguments: this.args,
+                applicationSettingsDirectory: 'settings',
+                packageReferences: packageReferences[this.options.platform],
+            })
+        );
+    }
 
-  writing() {
-    this._copyBotTemplateFiles();
-  }
+    writing() {
+        this._copyBotTemplateFiles();
+    }
 };
--- generators/generator-bot-enterprise-people/generators/app/index.js
+++ generators/generator-bot-enterprise-people/generators/app/index.js
@@ -6,29 +6,29 @@
 const { BaseGenerator } = require('@microsoft/generator-bot-adaptive');
 
 module.exports = class extends BaseGenerator {
-  initializing() {
-    this.composeWith(
-      require.resolve('@microsoft/generator-bot-adaptive/generators/app'),
-      Object.assign(this.options, {
-        arguments: this.args,
-        applicationSettingsDirectory: 'settings',
-        packageReferences: [
-          {
-            isPlugin: true,
-            name: 'Microsoft.Bot.Components.Graph',
-            version: '1.2.0-rc2',
-          },
-        ],
-        modifyApplicationSettings: (appSettings) => {
-          Object.assign(appSettings, {
-            oauthConnectionName: '',
-          });
-        },
-      })
-    );
-  }
+    initializing() {
+        this.composeWith(
+            require.resolve('@microsoft/generator-bot-adaptive/generators/app'),
+            Object.assign(this.options, {
+                arguments: this.args,
+                applicationSettingsDirectory: 'settings',
+                packageReferences: [
+                    {
+                        isPlugin: true,
+                        name: 'Microsoft.Bot.Components.Graph',
+                        version: '1.2.0',
+                    },
+                ],
+                modifyApplicationSettings: (appSettings) => {
+                    Object.assign(appSettings, {
+                        oauthConnectionName: '',
+                    });
+                },
+            })
+        );
+    }
 
-  writing() {
-    this._copyBotTemplateFiles();
-  }
+    writing() {
+        this._copyBotTemplateFiles();
+    }
 };
