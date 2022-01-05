16e2acd488cfc2e0ce17dc3891032c5510a4055f
Autumn Wyborny <auwyb@microsoft.com>
2022-01-05 14:21:57 -0800

    flattened regex batching and prompt with timeout changes

---
+++ packages/Telephony/Common/IStateMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Components.Telephony.Common
+{
+    internal enum StateStatus
+    {
+        /// <summary>
+        /// Represents an item that is currently running.
+        /// </summary>
+        Running,
+
+        /// <summary>
+        /// Represents an item that is currently completed.
+        /// </summary>
+        Completed
+    }
+
+    /// <summary>
+    /// Represents methods for accessing a state matrix tracking the current status of unique id'd states.
+    /// </summary>
+    internal interface IStateMatrix
+    {
+        Task<StateStatus> GetStatusForIdAsync(string id);
+
+        Task CompleteAsync(string id);
+
+        Task StartAsync(string id);
+
+        Task RunForStatusAsync(string id, StateStatus status, Func<Task> action);
+    }
+}
