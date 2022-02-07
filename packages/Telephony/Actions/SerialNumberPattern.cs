7bd13af7df3d0063d1e34814b31fa7e8d6e04bd3
Tien Suwandy <tiens@microsoft.com>
2022-02-06 23:30:31 -0800

    Added  SerialNumberPattern classes
    Added  SerialNumberPattern classes to post process STT result for serial numbers

---
+++ packages/Telephony/Actions/SerialNumberPattern.cs
@@ -0,0 +1,511 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Microsoft.Bot.Components.Telephony.Actions
+{
+    public class SerialNumberPattern
+    {
+        private static readonly Dictionary<char, char> AlphabetReplacementsTable = new Dictionary<char, char> { };
+        private static readonly Dictionary<char, char> DigitReplacementsTable = new Dictionary<char, char>
+        {
+            { 'A', '8' }
+        };
+
+        private static readonly Dictionary<string, char> DigitWordReplacementsTable = new Dictionary<string, char>
+        {
+            { "ZER0", '0' },
+            { "ONE", '1' },
+            { "TWO", '2' },
+            { "THREE", '3' },
+            { "FOR", '4' },
+            { "FOUR", '4' },
+            { "FIVE", '5' },
+            { "SIX", '6' },
+            { "SEVEN", '7' },
+            { "EIGHT", '8' },
+            { "NINE", '9' }
+        };
+
+        public SerialNumberPattern(IReadOnlyCollection<TextGroup> textGroups, string input)
+        {
+            Groups = textGroups;
+            InputString = input;
+
+            foreach (TextGroup group in Groups)
+            {
+                PatternLength += group.LengthInChars;
+            }
+        }
+
+        public SerialNumberPattern(string input)
+        {
+            // TODO: Make/set groups from input regex
+            Groups = new ReadOnlyCollection<TextGroup>(new List<TextGroup>());
+            InputString = input;
+        }
+
+        /// <summary>
+        /// Enum representing valid token type specified in pattern.
+        /// </summary>
+        public enum Token 
+        {
+            /// <summary>
+            /// Invalid token.
+            /// </summary>
+            Invalid,
+
+            /// <summary>
+            /// Alphabetic token.
+            /// </summary>
+            Alpha,
+
+            /// <summary>
+            /// Numeric token.
+            /// </summary>
+            Digit,
+
+            /// <summary>
+            /// Alphanumeric token.
+            /// </summary>
+            Both,
+
+            /// <summary>
+            /// The char - token.
+            /// </summary>
+            Dash
+        }
+
+        /// <summary>
+        /// Enum representing character replacement.
+        /// </summary>
+        public enum FixupType
+        {
+            /// <summary>
+            /// No replacement.
+            /// </summary>
+            None = 0,
+
+            /// <summary>
+            /// Character replacement.
+            /// </summary>
+            AlphaMapping = 1,
+
+            /// <summary>
+            /// As in replacement.
+            /// </summary>
+            AsIn = 2,
+        }
+
+        public string Regexp
+        {
+            get
+            {
+                string result = string.Empty;
+                foreach (TextGroup group in Groups)
+                {
+                    result += group.RegexString;
+                }
+
+                return result;
+            }
+        }
+
+        public string InputString { get; set; }
+
+        public IReadOnlyCollection<TextGroup> Groups { get; set; }
+
+        public int PatternLength { get; set; }
+
+        public Token PatternAt(int patternIndex)
+        {
+            int cumulativeIndex = 0;
+            int prevGroupCumulative = 0;
+            foreach (TextGroup group in Groups)
+            {
+                cumulativeIndex += group.LengthInChars;
+                if (cumulativeIndex > patternIndex)
+                {
+                    if (group.AcceptsDigits && group.AcceptsAlphabet)
+                    {
+                        return Token.Both;
+                    }
+
+                    if (group.AcceptsDigits)
+                    {
+                        return Token.Digit;
+                    }
+
+                    if (group.AcceptsAlphabet)
+                    {
+                        return Token.Alpha;
+                    }
+                }
+
+                prevGroupCumulative += group.LengthInChars;
+            }
+
+            return Token.Invalid;
+        }
+
+        public (char First, char Second) AmbiguousOptions(int inputIndex)
+        {
+            (char, char) result = ('*', '*');
+            char input = InputString[inputIndex];
+
+            switch (input)
+            {
+                case '8':
+                    result = ('A', input);
+                    break;
+                default:
+                    Debug.Assert(false, "No ambiguous input is found");
+                    break;
+            }
+
+            return result;
+        }
+
+        public bool DetectDigitFixup(int inputIndex)
+        {
+            char ch = InputString[inputIndex];
+
+            // Handle (One) = 1
+            string restOfInput = InputString.Substring(inputIndex);
+            string firstToken = restOfInput.Split(' ').FirstOrDefault();
+            if (firstToken != null)
+            {
+                string token = firstToken;
+                if (DigitWordReplacementsTable.ContainsKey(token))
+                {
+                    return true;
+                }
+            }
+
+            // Handle (A) = 8
+            return DigitReplacementsTable.ContainsKey(ch);
+        }
+
+        public char DigitFixup(int inputIndex, ref int newOffset)
+        {
+            char ch = InputString[inputIndex];
+            char replacement = char.MinValue;
+
+            // Handle (One) = 1
+            string restOfInput = InputString.Substring(inputIndex);
+            string firstToken = restOfInput.Split(' ').FirstOrDefault();
+            if (firstToken != null)
+            {
+                string token = firstToken;
+                if (DigitWordReplacementsTable.ContainsKey(token))
+                {
+                    replacement = DigitWordReplacementsTable[token];
+                    newOffset = inputIndex + token.Length;
+                }
+            }
+
+            // Handle (A) = 8
+            if (DigitReplacementsTable.ContainsKey(ch))
+            {
+                replacement = DigitReplacementsTable[ch];
+            }
+
+            Debug.WriteLine($"Digit {ch} was replaced by character {replacement}");
+            return replacement;
+        }
+
+        public FixupType DetectAlphabetFixup(int inputIndex)
+        {
+            char ch = InputString[inputIndex];
+            string restOfInput = InputString.Substring(inputIndex);
+
+            // (A as in Apple)BC
+            // ABC, as in Charlie Z as in Zeta.  
+            // [A-Z]{3}
+            var asInResult = FindAsInFixup(restOfInput);
+            if (asInResult.FixedUp)
+            {
+                return FixupType.AsIn;
+            }
+
+            // Find direct letter mapping
+            return AlphabetReplacementsTable.ContainsKey(ch) ? FixupType.AlphaMapping : FixupType.None;
+        }
+
+        public char AlphabetFixup(int inputIndex, ref int offset)
+        {
+            char ch = InputString[inputIndex];
+            string restOfInput = InputString.Substring(inputIndex);
+            offset = 1;
+            switch (DetectAlphabetFixup(inputIndex))
+            {
+                case FixupType.None:
+                    return ch;
+                case FixupType.AlphaMapping:
+                    char replacement = AlphabetReplacementsTable[ch];
+                    Console.WriteLine($"Alphabetic Character {ch} was replaced by digit {replacement}");
+                    return replacement;
+                case FixupType.AsIn:
+                    AsInResult asInResult;
+                    asInResult = FindAsInFixup(restOfInput);
+                    if (asInResult.FixedUp)
+                    {
+                        Debug.WriteLine($"'as in' fixed up {restOfInput} to letter {asInResult.Char}");
+                        offset = asInResult.NewOffset;
+                        return asInResult.Char;
+                    }
+
+                    throw new Exception("Should have returned a char described by as in");
+            }
+
+            // TODO: Do additional fixups here
+            return ch;
+        }
+
+        // Pattern : 2 alphabetic, 1 numeric
+        // Input:    katie 4
+        public string[] Inference()
+        {
+            List<string> results = new List<string>();
+            Console.WriteLine($"Original text      : '{InputString}'");
+            Console.WriteLine($"Regular Expression : '{Regexp}'");
+
+            // Trivial Length check - must be at least pattern length (most likely longer).
+            if (InputString.Length < PatternLength)
+            {
+                Console.WriteLine($"Input string is too short!  Must be at least {PatternLength} characters/digits.");
+                return results.ToArray();
+            }
+
+            int patternIndex = 0;
+            int inputIndex = 0;
+
+            List<int> ambiguousInputIndexes = new List<int>();
+            bool isMatch = true;
+            string fixedUpString = string.Empty;
+
+            // Initial Scan to see how many things to correct
+            while (patternIndex < PatternLength && inputIndex < InputString.Length)
+            {
+                var elementType = PatternAt(patternIndex);    // What type the pattern is expecting
+                var inputElement = InputString[inputIndex];
+
+                // Skip white space, period, comma, dash if not expected
+                if (inputElement == ' ' || inputElement == '.' || inputElement == ',' || 
+                    (inputElement == '-' && elementType != Token.Dash))
+                {
+                    inputIndex++;
+                    continue;
+                }
+                
+                Debug.WriteLine($"Token at index {patternIndex} is {elementType}");
+                Debug.WriteLine($"Input at index {inputIndex} is {inputElement}");
+
+                patternIndex++;  // Bump to next element in pattern.
+
+                var inferResult = InferMatch(inputIndex, elementType);
+
+                if (inferResult.IsAmbiguous)
+                {
+                    // Store ambiguity index from original string.
+                    ambiguousInputIndexes.Add(inputIndex);
+                    fixedUpString += '*'; // Mark ambiguity
+                }
+                else if (inferResult.IsFixedUp)
+                {
+                    fixedUpString += inferResult.Ch;
+                    inputIndex += inferResult.NewOffset - 1;
+                }
+                else
+                {
+                    fixedUpString += inputElement;
+                }
+
+                if (inferResult.IsNoMatch)
+                {
+                    Console.WriteLine("ERROR: No match");
+                    isMatch = false;
+                    break;
+                }
+                
+                inputIndex++;
+            }
+
+            if (!isMatch)
+            {
+                return results.ToArray();
+            }
+
+            // Handle ambiguous issues.
+            if (ambiguousInputIndexes.Count > 0)
+            {
+#pragma warning disable IDE0042 // Deconstruct variable declaration
+                var options = AmbiguousOptions(ambiguousInputIndexes.FirstOrDefault());
+#pragma warning restore IDE0042 // Deconstruct variable declaration
+                results.Add(fixedUpString.Replace('*', options.First));
+                results.Add(fixedUpString.Replace('*', options.Second));
+            }
+            else
+            {
+                results.Add(fixedUpString);
+            }
+     
+            return results.ToArray();
+        }
+
+        private AmbiguousResult CheckAmbiguous(ref int inputIndex)
+        {
+            AmbiguousResult match = new AmbiguousResult();
+            char input = InputString[inputIndex];
+            match.Ch = input;
+
+            switch (input)
+            {
+                case '8':
+                    match.IsAmbiguous = true;
+                    return match;
+                default:
+                    break;
+            }
+
+            return match;
+        }
+
+        private AsInResult FindAsInFixup(string input)
+        {
+            AsInResult result = new AsInResult();
+            char[] delimiters = { ' ', ',', '.', '/', '-' };
+            var tokens = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            if (tokens.Length > 3 && tokens[0].Length == 1)
+            {
+                char proposedChar = '*';
+                if (tokens[1].ToLowerInvariant() == "as" && tokens[2].ToLowerInvariant() == "in")
+                {
+                    proposedChar = tokens[3][0];
+                }
+
+                Debug.WriteLine($"'as in' detected character {proposedChar} as the proposed character replacement.");
+                result.FixedUp = true;
+                result.Char = proposedChar;
+                int initialOffset = input.IndexOf(" in ", StringComparison.Ordinal);
+                result.NewOffset = initialOffset + 4 + tokens[3].Length;
+            }
+
+            return result;
+        }
+
+        private InferResult InferMatch(int inputIndex, Token elementType)
+        {
+            InferResult result = new InferResult();
+            char currentInputChar = InputString[inputIndex];
+            result.Ch = currentInputChar;
+
+            switch (elementType)
+            {
+                case Token.Digit:
+                    TryFixupDigit(inputIndex, currentInputChar, elementType, result);
+                    break;
+                case Token.Alpha:
+                    if (char.IsLetter(currentInputChar) == false && DetectAlphabetFixup(inputIndex) == FixupType.None)
+                    {
+                        Console.WriteLine($"ERROR: Element index {inputIndex + 1} (character {currentInputChar}) is not alpha (Pattern wants {elementType})");
+                        result.IsNoMatch = true;
+                    }
+                    else
+                    {
+                        int newOffset = 1;
+                        result.IsFixedUp = true;
+                        result.Ch = AlphabetFixup(inputIndex, ref newOffset);
+                        result.NewOffset = newOffset;
+                    }
+
+                    break;
+                case Token.Both:
+                    AmbiguousResult ambiguousResult = CheckAmbiguous(ref inputIndex);
+                    result.Ch = ambiguousResult.Ch;
+                    if (ambiguousResult.IsAmbiguous)
+                    {
+                        result.IsAmbiguous = true;
+                        Console.WriteLine($"INFO: Element index {inputIndex + 1}(character {InputString[inputIndex]}) is ambiguous (Pattern wants {elementType})");
+                    }
+                    else
+                    {
+                        TryFixupDigit(inputIndex, currentInputChar, elementType, result);
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+
+            return result;
+        }
+
+        private void TryFixupDigit(int inputIndex, char currentInputChar, Token elementType, InferResult result)
+        {
+            int newOffset = 1;
+            result.IsFixedUp = DetectDigitFixup(inputIndex);
+
+            if (char.IsDigit(currentInputChar) == false && !result.IsFixedUp)
+            {
+                if (elementType == Token.Digit)
+                {
+                    Console.WriteLine($"ERROR: Element index {inputIndex + 1} (character {currentInputChar}) is not digit (Pattern wants {elementType})");
+                    result.IsNoMatch = true;
+                }            
+            }
+            else if (result.IsFixedUp)
+            {
+                result.Ch = DigitFixup(inputIndex, ref newOffset);
+                result.NewOffset = newOffset;
+            }
+        }
+
+        private class AmbiguousResult
+        {
+            public char? Ch { get; set; }
+
+            public bool IsAmbiguous { get; set; }
+        }
+
+        private class AsInResult
+        {
+            public AsInResult()
+            {
+                NewOffset = 1;
+                Char = '*';
+            }
+
+            public bool FixedUp { get; set; }
+
+            public int NewOffset { get; set; }
+
+            public char Char { get; set; }
+        }
+
+        private class InferResult
+        {
+            public InferResult()
+            {
+                NewOffset = 1;
+            }
+
+            public char? Ch { get; set; }
+
+            public bool IsFixedUp { get; set; }
+
+            public bool IsAmbiguous { get; set; }
+
+            public bool IsNoMatch { get; set; }
+
+            public int NewOffset { get; set; }
+        }
+    }
+}
---
+++ packages/Telephony/Actions/SerialNumberTextGroup.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+
+namespace Microsoft.Bot.Components.Telephony.Actions
+{
+    public class TextGroup
+    {
+        public bool AcceptsAlphabet { get; set; } = false;
+
+        public bool AcceptsDigits { get; set; } = false;
+
+        public short LengthInChars { get; set; } = 0;
+
+        public string Regex { get; set; } = string.Empty;
+
+        public string RegexString
+        {
+            get
+            {
+                string result = "([";
+                if (AcceptsDigits)
+                {
+                    result += "0-9";
+                }
+
+                if (AcceptsAlphabet)
+                {
+                    result += "a-zA-Z";
+                }
+
+                result += "]{" + LengthInChars + "})";
+                return result;
+            }
+        }
+
+        public string GenerateGroupExampleText()
+        {
+            string seed = string.Empty;
+            Random rnd = new Random();
+            if (AcceptsAlphabet)
+            {
+                seed += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+
+            if (AcceptsDigits)
+            {
+                seed += "0123456789";
+            }
+
+            return new string(Enumerable.Repeat(seed, LengthInChars)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+    }
+}
