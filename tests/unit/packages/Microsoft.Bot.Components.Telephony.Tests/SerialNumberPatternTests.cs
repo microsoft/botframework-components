e93e774061b282551da1740e296b1c21ce81616f
stevengum <14935595+stevengum@users.noreply.github.com>
2022-11-16 20:38:36 -0800

    add SNP Tests, minor cleanup

--- tests/unit/packages/Microsoft.Bot.Components.Telephony.Tests/SerialNumberPatternTests.cs
+++ tests/unit/packages/Microsoft.Bot.Components.Telephony.Tests/SerialNumberPatternTests.cs
@@ -8,23 +8,35 @@ using Xunit;
 
 namespace Microsoft.Bot.Components.Telephony.Tests
 {
-    public class TextGroupTests
+    public class SerialNumberPatternTests
     {
         [Theory]
         [MemberData(nameof(ConstructorThrowsData))]
         public void TextGroup_Ctor_Throws_With_Bad_Groups(string badRegexPattern)
         {
-            Assert.Throws<ArgumentException>(() => new TextGroup(badRegexPattern));
+            Assert.Throws<ArgumentException>(() => new SerialNumberPattern(badRegexPattern));
         }
 
         public static IEnumerable<object[]> ConstructorThrowsData => new List<object[]>
         {
-            new object[] { string.Empty },
             new object[] { "(][){}" },
             new object[] { "([]{})" },
             new object[] { "(}{)[2]" },
             new object[] { "(}{)[2a]" },
             new object[] { "([2]{})" },
+        };
+
+
+        [Theory]
+        [MemberData(nameof(PatternLengthData))]
+        public void PatternLength_Includes_All_Lengths(string regexPattern, int patternLength)
+        {
+            Assert.Equal(patternLength, new SerialNumberPattern(regexPattern).PatternLength);
+        }
+
+        public static IEnumerable<object[]> PatternLengthData => new List<object[]>
+        {
+            new object[] { "([0-9]{6})", 6 }
         };
     }
 }
