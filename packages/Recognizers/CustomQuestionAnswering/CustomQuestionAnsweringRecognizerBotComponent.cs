c9463e35323853d3a582d8c8974cd0029d83bbb1
dasvictor <victor.iitk@gmail.com>
2022-12-07 22:42:57 +0530

    feat: CQA recognizers (#1405)
    * Fix duplicate reference
    
    * CQA recognizer
    
    * csproj and kind updates
    
    * package reference url

---
+++ packages/Recognizers/CustomQuestionAnswering/CustomQuestionAnsweringRecognizerBotComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Bot.Components.Recognizers
+{
+    public class CustomQuestionAnsweringRecognizerBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<DeclarativeType>((sp) => new DeclarativeType<CustomQuestionAnsweringRecognizer>(CustomQuestionAnsweringRecognizer.Kind));
+        }
+    }
+}
