4fba57cd05e27e578508d2be56722cc807ca6417
Steven Gum <14935595+stevengum@users.noreply.github.com>
2022-12-07 09:15:03 -0800

    fix: Fix aggregation bug in SerialNumberInput and update its configurable properties (#1389)
    * initial cleanup in Telephony package, add TextGroupTests.cs
    
    * fix instantiation bug in Telephony cleanup
    
    * add SNP Tests, minor cleanup
    
    * fix bug(?) in SerialNumberInput.schema required fields
    
    * fix aggregation bug and simplify authoring experience for SerialNumberInput
    
    * address TODO for circuitbreaker logic & other PR feedback

---
+++ tests/unit/packages/Microsoft.Bot.Components.Telephony.Tests/TextGroupTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Components.Telephony.Actions;
+using Xunit;
+
+namespace Microsoft.Bot.Components.Telephony.Tests
+{
+    public class TextGroupTests
+    {
+        [Theory]
+        [MemberData(nameof(ConstructorThrowsData))]
+        public void TextGroup_Ctor_Throws_With_Bad_Groups(string badRegexPattern)
+        {
+            Assert.Throws<ArgumentException>(() => new TextGroup(badRegexPattern));
+        }
+
+        public static IEnumerable<object[]> ConstructorThrowsData => new List<object[]>
+        {
+            new object[] { string.Empty },
+            new object[] { "(][){}" },
+            new object[] { "([]{})" },
+            new object[] { "(}{)[2]" },
+            new object[] { "(}{)[2a]" },
+            new object[] { "([2]{})" },
+        };
+    }
+}
