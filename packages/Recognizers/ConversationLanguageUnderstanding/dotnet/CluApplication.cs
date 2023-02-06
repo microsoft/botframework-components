a971767db89e7a1335f0aaf3d5df135b933c1993
Orestis Meikopoulos <diktyas1988@gmail.com>
2023-02-06 16:48:05 +0200

    Initial commit of CLU recognizer package.

---
+++ packages/Recognizers/ConversationLanguageUnderstanding/dotnet/CluApplication.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Microsoft.Bot.Components.Recognizers.CLURecognizer
+{
+    /// <summary>
+    /// Data describing a CLU application.
+    /// </summary>
+    public class CluApplication
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CluApplication"/> class.
+        /// </summary>
+        /// <param name="projectName">CLU project name.</param>
+        /// <param name="endpointKey">CLU subscription or endpoint key.</param>
+        /// <param name="endpoint">CLU endpoint to use.</param>
+        /// <param name="deploymentName">CLU deployment name.</param>
+        public CluApplication(string projectName, string endpointKey, string endpoint, string deploymentName)
+            : this((projectName, endpointKey, endpoint, deploymentName))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CluApplication"/> class.
+        /// For unit tests only.
+        /// </summary>
+        protected CluApplication()
+        {
+        }
+
+        private CluApplication(ValueTuple<string, string, string, string> props)
+        {
+            var (projectName, endpointKey, endpoint, deploymentName) = props;
+
+            if (string.IsNullOrWhiteSpace(projectName))
+            {
+                throw new ArgumentException($"CLU \"{nameof(projectName)}\" parameter cannot be null or empty.");
+            }
+
+            if (!Guid.TryParse(endpointKey, out var _))
+            {
+                throw new ArgumentException($"\"{endpointKey}\" is not a valid CLU subscription key.");
+            }
+
+            if (string.IsNullOrWhiteSpace(endpoint))
+            {
+                throw new ArgumentException($"CLU \"{nameof(endpoint)}\" parameter cannot be null or empty.");
+            }
+
+            if (!Uri.IsWellFormedUriString(endpoint, UriKind.Absolute))
+            {
+                throw new ArgumentException($"\"{endpoint}\" is not a valid CLU endpoint.");
+            }
+
+            if (string.IsNullOrWhiteSpace(deploymentName))
+            {
+                throw new ArgumentException($"CLU \"{nameof(deploymentName)}\" parameter cannot be null or empty.");
+            }
+
+            ProjectName = projectName;
+            EndpointKey = endpointKey;
+            Endpoint = endpoint;
+            DeploymentName = deploymentName;
+        }
+
+        /// <summary>
+        /// Gets or sets CLU project name.
+        /// </summary>
+        /// <value>
+        /// CLU project name.
+        /// </value>
+        public string ProjectName { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets CLU subscription or endpoint key.
+        /// </summary>
+        /// <value>
+        /// CLU subscription or endpoint key.
+        /// </value>
+        public string EndpointKey { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets CLU endpoint.
+        /// </summary>
+        /// <value>
+        /// CLU endpoint where application is hosted.
+        /// </value>
+        public string Endpoint { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets CLU deployment name.
+        /// </summary>
+        /// <value>
+        /// CLU deployment name.
+        /// </value>
+        public string DeploymentName { get; set; } = default!;
+    }
+}
---
+++ packages/Recognizers/ConversationLanguageUnderstanding/dotnet/DefaultHttpClientFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Rest;
+
+namespace Microsoft.Bot.Components.Recognizers.CLURecognizer
+{
+    /// <summary>
+    /// HttpClientFactory that always returns the same HttpClient instance for CLU calls.
+    /// </summary>
+    internal class DefaultHttpClientFactory : IHttpClientFactory
+    {
+        private static readonly HttpClient _httpClient = new HttpClient(CreateHttpHandlerPipeline(CreateRootHandler(), new CluDelegatingHandler()), false)
+        {
+            Timeout = TimeSpan.FromMilliseconds(CluConstants.HttpClientOptions.Timeout),
+        };
+        
+        /// <summary>
+        /// Returns the same default HttpClient instance.
+        /// </summary>
+        /// <param name="name">Name is not used in this context. This parameter is here, because it is dictated by the <see cref="IHttpClientFactory"/> interface method declaration.</param>
+        /// <returns>The same HttpClient instance.</returns>
+        public HttpClient CreateClient(string name)
+        {
+            return _httpClient;
+        }
+
+        private static HttpClientHandler CreateRootHandler() => new HttpClientHandler();
+
+        private static DelegatingHandler CreateHttpHandlerPipeline(HttpClientHandler httpClientHandler, params DelegatingHandler[] handlers)
+        {
+            // Now, the RetryAfterDelegatingHandler should be the absolute outermost handler
+            // because it's extremely lightweight and non-interfering
+            DelegatingHandler currentHandler =
+#pragma warning disable CA2000 // Dispose objects before losing scope (suppressing this warning, for now! we will address this once we implement HttpClientFactory in a future release)
+                new RetryDelegatingHandler(
+                    new RetryAfterDelegatingHandler 
+                    { 
+                        InnerHandler = httpClientHandler 
+                    });
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+            if (handlers != null)
+            {
+                for (var i = handlers.Length - 1; i >= 0; --i)
+                {
+                    var handler = handlers[i];
+
+                    // Non-delegating handlers are ignored since we always
+                    // have RetryDelegatingHandler as the outer-most handler
+                    while (handler!.InnerHandler is DelegatingHandler)
+                    {
+                        handler = handler.InnerHandler as DelegatingHandler;
+                    }
+
+                    handler.InnerHandler = currentHandler;
+
+                    currentHandler = handlers[i];
+                }
+            }
+
+            return currentHandler;
+        }
+    }
+}
