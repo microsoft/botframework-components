86e2135e10c587fcee1e8c9cf963b108c4d80dfc
tracyboehrer <tracyboehrer@users.noreply.github.com>
2023-03-22 12:18:10 -0500

    bump: SDK to 4.19.3 (JS & C#) (#1484)
    * CLU using 4.18.1 for RC3
    
    * JS SDK to 4.19.3
    
    * CLU Recognizer to SDK 4.19.3
    
    * Update yarn.lock
    
    ---------
    
    Co-authored-by: Tracy Boehrer <trboehre@microsoft.com>

--- generators/generator-bot-adaptive/platforms.js
+++ generators/generator-bot-adaptive/platforms.js
@@ -8,7 +8,7 @@ const dotnet = {
 
 const js = {
   name: 'js',
-  defaultSdkVersion: '4.19.2-preview',
+  defaultSdkVersion: '4.19.3-preview',
 };
 
 module.exports = { dotnet, js };
--- generators/generator-bot-adaptive/test/js-webapp.test.js
+++ generators/generator-bot-adaptive/test/js-webapp.test.js
@@ -86,10 +86,10 @@ describe(`generator-bot-adaptive --platform ${platform} --integration ${integrat
       },
       dependencies: {
         'cross-env': 'latest',
-        'botbuilder-ai-luis': '4.19.2-preview',
-        'botbuilder-ai-qna': '4.19.2-preview',
+        'botbuilder-ai-luis': '4.19.3-preview',
+        'botbuilder-ai-qna': '4.19.3-preview',
         'botbuilder-dialogs-adaptive-runtime-integration-express':
-          '4.19.2-preview',
+          '4.19.3-preview',
       },
     };
 
